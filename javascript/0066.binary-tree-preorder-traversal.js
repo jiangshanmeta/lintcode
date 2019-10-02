e907aefae5e95b91733453827757a1d51c58417a
jiangshanmeta <540118044@qq.com>
2019-10-03 00:19:51 +0800

    lintcode 66

---
+++ javascript/0066.binary-tree-preorder-traversal.js
@@ -0,0 +1,15 @@
+/**
+ * @param root: A Tree
+ * @return: Preorder in ArrayList which contains node values.
+ */
+const preorderTraversal = function (root) {
+    const result = [];
+    root && preOrder(root,result);
+    return result;
+}
+
+function preOrder(root,result){
+    result.push(root.val);
+    root.left && preOrder(root.left,result);
+    root.right && preOrder(root.right,result);
+}
