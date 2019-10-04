156ae7f2eb79e92224af5a53291fe957d68a901a
jiangshanmeta <540118044@qq.com>
2019-10-04 13:45:25 +0800

    lintcode 37

---
+++ javascript/0037.reverse-3-digit-integer.js
@@ -0,0 +1,14 @@
+/**
+ * @param number: A 3-digit number.
+ * @return: Reversed number.
+ */
+const reverseInteger = function (number) {
+    let result = 0;
+    while(number){
+        const digit = number%10;
+        result = result*10+digit;
+        number = (number-digit)/10;
+    }
+
+    return result;
+}
