6024690e4fb0c822b8997c11cd37c88fd61f9ca6
jiangshanmeta <540118044@qq.com>
2019-10-05 12:54:46 +0800

    lintcode 0035

---
+++ javascript/0035.reverse-linked-list.js
@@ -0,0 +1,14 @@
+/**
+ * @param head: n
+ * @return: The new head of reversed linked list.
+ */
+const reverse = function (head) {
+    const dummyHead = new ListNode();
+    while(head){
+        const next = head.next;
+        head.next = dummyHead.next;
+        dummyHead.next = head;
+        head = next;
+    }
+    return dummyHead.next;
+}
