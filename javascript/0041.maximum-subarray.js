43410f502492403701a2062eb3563195b599d5c9
jiangshanmeta <540118044@qq.com>
2019-10-06 08:01:06 +0800

    lintcode 0041

---
+++ javascript/0041.maximum-subarray.js
@@ -0,0 +1,18 @@
+/**
+ * @param nums: A list of integers
+ * @return: A integer indicate the sum of max subarray
+ */
+const maxSubArray = function (nums) {
+    let result = nums[0];
+    let curSum = nums[0];
+    for(let i=1;i<nums.length;i++){
+        if(curSum<0){
+            curSum = nums[i];
+        }else{
+            curSum += nums[i];
+        }
+        result = Math.max(result,curSum);
+    }
+
+    return result;
+}
