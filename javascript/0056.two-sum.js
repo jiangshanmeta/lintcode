79c26e3456ec07db6fd8a6347d3327fce5886869
jiangshanmeta <540118044@qq.com>
2019-10-07 21:34:38 +0800

    lintcode 56

---
+++ javascript/0056.two-sum.js
@@ -0,0 +1,14 @@
+/**
+ * @param numbers: An array of Integer
+ * @param target: target = numbers[index1] + numbers[index2]
+ * @return: [index1, index2] (index1 < index2)
+ */
+const twoSum = function (numbers, target) {
+    const map = {};
+    for(let i=0;i<numbers.length;i++){
+        if(map[target-numbers[i]] !== undefined){
+            return [map[target-numbers[i]],i];
+        }
+        map[numbers[i]] = i;
+    }
+}
