2c2f18c69e59f509c709ad579990a19662ae047b
jiangshanmeta <540118044@qq.com>
2019-10-07 21:34:46 +0800

    lintcode 60

---
+++ javascript/0060.search-insert-position.js
@@ -0,0 +1,20 @@
+/**
+ * @param A: an integer sorted array
+ * @param target: an integer to be inserted
+ * @return: An integer
+ */
+const searchInsert = function (A, target) {
+    let result = A.length;
+    let low = 0;
+    let high = A.length -1 ;
+    while(low<=high){
+        const mid = (low+high) >> 1;
+        if(A[mid]<target){
+            low = mid+1;
+        }else{
+            result = Math.min(result,mid);
+            high = mid-1;
+        }
+    }
+    return result;
+}
