b33bbfee902754e24786471b34b3c2c21b149ec5
jiangshanmeta <540118044@qq.com>
2019-10-10 01:05:10 +0800

    lintcode 0082

---
+++ javascript/0082.single-number.js
@@ -0,0 +1,12 @@
+/**
+ * @param A: An integer array
+ * @return: An integer
+ */
+const singleNumber = function (A) {
+    let result = 0;
+    for(let i=0;i<A.length;i++){
+        result = result^A[i];
+    }
+    return result;
+}
+
