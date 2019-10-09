7636639e835840586bee31e05edf797996b2be3f
jiangshanmeta <540118044@qq.com>
2019-10-10 01:05:01 +0800

    lintcode 0073

---
+++ javascript/0073.construct-binary-tree-from-preorder-and-inorder-traversal.js
@@ -0,0 +1,44 @@
+/**
+ * @param preorder: A list of integers that preorder traversal of a tree
+ * @param inorder: A list of integers that inorder traversal of a tree
+ * @return: Root of a tree
+ */
+const buildTree = function (preorder, inorder) {
+    return buildTreeByIndexs(preorder,inorder,0,preorder.length-1,0,inorder.length-1);
+}
+
+function buildTreeByIndexs(preorder,inorder,preorderStartIndex,preorderEndIndex,inorderStartIndex,inorderEndIndex){
+    if(preorderStartIndex>preorderEndIndex){
+        return null;
+    }
+
+    const rootVal = preorder[preorderStartIndex];
+
+    let rootIndexInInOrder;
+    for(let i=inorderStartIndex;i<inorderEndIndex+1;i++){
+        if(inorder[i] === rootVal){
+            rootIndexInInOrder = i;
+            break;
+        }
+    }
+    const root = new TreeNode(rootVal);
+    root.left = buildTreeByIndexs(
+        preorder,
+        inorder,
+        preorderStartIndex+1,
+        rootIndexInInOrder-inorderStartIndex+preorderStartIndex,
+        inorderStartIndex,
+        rootIndexInInOrder-1
+    );
+
+    root.right = buildTreeByIndexs(
+        preorder,
+        inorder,
+        preorderEndIndex+rootIndexInInOrder-inorderEndIndex+1,
+        preorderEndIndex,
+        rootIndexInInOrder+1,
+        inorderEndIndex
+    );
+
+    return root;
+}
