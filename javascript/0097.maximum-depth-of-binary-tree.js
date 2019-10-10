bfd49b44bd9f022457ecbcdb9036af3b5f52c360
jiangshanmeta <540118044@qq.com>
2019-10-11 00:31:20 +0800

    lintcode 0097

---
+++ javascript/0097.maximum-depth-of-binary-tree.js
@@ -0,0 +1,10 @@
+/**
+ * @param root: The root of binary tree.
+ * @return: An integer
+ */
+const maxDepth = function (root) {
+    if(!root){
+        return 0;
+    }
+    return Math.max(maxDepth(root.left),maxDepth(root.right))+1;
+}
