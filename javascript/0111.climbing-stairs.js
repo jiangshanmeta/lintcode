9a2ddbe9c1f9e0499c2dbc1862a36d5e4a156fec
jiangshanmeta <540118044@qq.com>
2019-10-12 00:21:36 +0800

    lintcode 0111

---
+++ javascript/0111.climbing-stairs.js
@@ -0,0 +1,20 @@
+/**
+ * @param n: An integer
+ * @return: An integer
+ */
+const climbStairs = function (n) {
+    if(n===0){
+        return 0;
+    }else if(n===1){
+        return 1;
+    }else if(n===2){
+        return 2;
+    }
+    let a = 1;
+    let b = 2;
+    for(let i=2;i<n;i++){
+        [a,b] = [b,a+b];
+    }
+    return b;
+}
+
