dbc898bf5a450d4ca3ad9592018ff956963d880a
jiangshanmeta <540118044@qq.com>
2019-10-12 00:21:22 +0800

    lintcode 0094

---
+++ javascript/0094.binary-tree-maximum-path-sum.js
@@ -0,0 +1,24 @@
+/**
+ * @param root: The root of binary tree.
+ * @return: An integer
+ */
+const maxPathSum = function (root) {
+    return helper(root)[0];
+}
+
+function helper(root){
+    if(!root){
+        return [-Infinity,-Infinity];
+    }
+    const [leftRst,leftMax] = helper(root.left);
+    const [rightRst,rightMax] = helper(root.right);
+    const curMax = Math.max(0,leftMax,rightMax)+root.val;
+    return [
+        Math.max(
+            leftRst,
+            rightRst,
+            root.val+(rightMax>0?rightMax:0)+(leftMax>0?leftMax:0)
+        ),
+        curMax,
+    ]
+}
