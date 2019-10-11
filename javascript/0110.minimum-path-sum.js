e0da8cf6d6e6af422ce110c4b345c9e58c702281
jiangshanmeta <540118044@qq.com>
2019-10-12 00:21:29 +0800

    lintcode 0110

---
+++ javascript/0110.minimum-path-sum.js
@@ -0,0 +1,19 @@
+/**
+ * @param grid: a list of lists of integers
+ * @return: An integer, minimizes the sum of all numbers along its path
+ */
+const minPathSum = function (grid) {
+    const dp = new Array(grid[0].length);
+    dp[0] = grid[0][0];
+    for(let i=1;i<grid[0].length;i++){
+        dp[i] = dp[i-1]+grid[0][i];
+    }
+    for(let i=1;i<grid.length;i++){
+        dp[0] += grid[i][0];
+        for(let j=1;j<grid[0].length;j++){
+            dp[j] = Math.min(dp[j-1],dp[j])+grid[i][j]
+        }
+    }
+    
+    return dp[grid[0].length-1];
+}
