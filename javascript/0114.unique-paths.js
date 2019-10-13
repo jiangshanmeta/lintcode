d369f0dd90a42071306e687e7bff65636efce443
jiangshanmeta <540118044@qq.com>
2019-10-14 00:13:46 +0800

    lintcode 0114

---
+++ javascript/0114.unique-paths.js
@@ -0,0 +1,14 @@
+/**
+ * @param m: positive integer (1 <= m <= 100)
+ * @param n: positive integer (1 <= n <= 100)
+ * @return: An integer
+ */
+const uniquePaths = function (m, n) {
+    const dp = new Array(m).fill(1);
+    for(let i=1;i<n;i++){
+        for(let j=1;j<m;j++){
+            dp[j] += dp[j-1];
+        }
+    }
+    return dp[m-1];
+}
