73506ac527bfeb0a77aad94f1c89119f26f0252e
jiangshanmeta <540118044@qq.com>
2019-10-14 00:13:38 +0800

    lintcode 0112

---
+++ javascript/0112.remove-duplicates-from-sorted-list.js
@@ -0,0 +1,19 @@
+/**
+ * @param head: head is the head of the linked list
+ * @return: head of linked list
+ */
+const deleteDuplicates = function (head) {
+    const dummyHead = {};
+    dummyHead.next = head;
+    let prev = dummyHead;
+    while(head){
+        if(head.val === prev.val){
+            prev.next = head.next;
+        }else{
+            prev = head;
+        }
+        head = head.next;
+    }
+
+    return dummyHead.next;
+}
