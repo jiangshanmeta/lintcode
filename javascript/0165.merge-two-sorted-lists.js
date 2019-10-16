4a4a6d1d8fa9b8a1cf2ab77451f20a61fc7ef8fd
jiangshanmeta <540118044@qq.com>
2019-10-17 00:25:26 +0800

    lintcode 0165

---
+++ javascript/0165.merge-two-sorted-lists.js
@@ -0,0 +1,27 @@
+/**
+ * @param l1: ListNode l1 is the head of the linked list
+ * @param l2: ListNode l2 is the head of the linked list
+ * @return: ListNode head of linked list
+ */
+const mergeTwoLists = function (l1, l2) {
+    const dummyHead = new ListNode();
+    let last = dummyHead;
+    while(l1 && l2){
+        if(l1.val<l2.val){
+            last.next = l1;
+            l1 = l1.next;
+        }else{
+            last.next = l2;
+            l2 = l2.next;
+        }
+        last = last.next;
+    }
+
+    if(l1){
+        last.next = l1;
+    }else{
+        last.next = l2;
+    }
+
+    return dummyHead.next;
+}
