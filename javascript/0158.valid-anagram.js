8997fe530bfe97f0f4c090a69eb03634fbe93778
jiangshanmeta <540118044@qq.com>
2019-10-16 13:18:31 +0800

    lintcode 0158

---
+++ javascript/0158.valid-anagram.js
@@ -0,0 +1,18 @@
+/**
+ * @param s: The first string
+ * @param t: The second string
+ * @return: true or false
+ */
+const anagram = function (s, t) {
+    if(s.length !== t.length){
+        return false;
+    }
+
+    const counts = new Array(26).fill(0);
+    for(let i=0;i<s.length;i++){
+        counts[s.charCodeAt(i)-97]++;
+        counts[t.charCodeAt(i)-97]--;
+    }
+    return counts.every(item=>item===0);
+}
+
