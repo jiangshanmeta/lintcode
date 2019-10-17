c2d43757df5f871b886de289e78e4d90f80604ef
jiangshanmeta <540118044@qq.com>
2019-10-18 00:20:07 +0800

    lintcode 0181

---
+++ javascript/0181.flip-bits.js
@@ -0,0 +1,36 @@
+/**
+ * @param a: An integer
+ * @param b: An integer
+ * @return: An integer
+ */
+const bitSwapRequired = function (a, b) {
+    // 要转成无符号的
+    a >>>= 0;
+    b >>>= 0;
+    let count = 0;
+    while(a && b){
+        const digit1 = a&1;
+        const digit2 = b&1;
+
+        if(digit1 !== digit2){
+            count++;
+        }
+
+        a >>>= 1;
+        b >>>= 1;
+    }
+
+    while(a){
+        const digit = a&1;
+        digit && count++;
+        a >>>= 1;
+    }
+
+    while(b){
+        const digit = b&1;
+        digit && count++;
+        b >>>= 1;
+    }
+
+    return count;
+}
