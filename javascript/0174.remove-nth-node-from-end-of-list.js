fc03a7587a73eb4083e338df1ee2b3b4f5b500b1
jiangshanmeta <540118044@qq.com>
2019-10-18 00:19:42 +0800

    lintcode 0174

---
+++ javascript/0174.remove-nth-node-from-end-of-list.js
@@ -0,0 +1,21 @@
+/**
+ * @param head: The first node of linked list.
+ * @param n: An integer
+ * @return: The head of linked list.
+ */
+const removeNthFromEnd = function (head, n) {
+    const dummyHead = new ListNode(0);
+    dummyHead.next = head;
+    last = dummyHead;
+    while(n--){
+        head = head.next;
+    }
+    while(head){
+        head = head.next;
+        last = last.next;
+    }
+    last.next = last.next.next;
+
+    return dummyHead.next;
+}
+
