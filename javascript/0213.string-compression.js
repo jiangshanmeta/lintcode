835650c83e07adccba8280b524067ec4e88f2d59
jiangshanmeta <540118044@qq.com>
2019-10-20 01:44:06 +0800

    lintcode 0213

---
+++ javascript/0213.string-compression.js
@@ -0,0 +1,18 @@
+/**
+ * @param originalString: a string
+ * @return: a compressed string
+ */
+const compress = function (originalString) {
+    const result = [];
+    let index = 0;
+    while(index<originalString.length){
+        const char = originalString[index++];
+        let count = 1;
+        while (index<originalString.length && originalString[index] === char){
+            count++;
+            index++;
+        }
+        result.push(char,count);
+    }
+    return result.length<originalString.length ? result.join('') : originalString;
+}
