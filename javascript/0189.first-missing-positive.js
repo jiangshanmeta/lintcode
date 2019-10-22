21681fc15f51a3ea174afc9728bb44187c5a4f0f
jiangshanmeta <540118044@qq.com>
2019-10-23 00:35:46 +0800

    lintcode 189

---
+++ javascript/0189.first-missing-positive.js
@@ -0,0 +1,29 @@
+/**
+ * @param A: An array of integers
+ * @return: An integer
+ */
+const firstMissingPositive = function (A) {
+    let index = 0;
+    while(index<A.length){
+        if(A[index] === index+1 || A[index]<1 || A[index]>A.length){
+            index++;
+            continue;
+        }
+        const m = A[index];
+        if(A[m-1] !== m){
+            const tmp = A[m-1];
+            A[m-1] = m;
+            A[index] = tmp;
+        }else{
+            index++;
+        }
+    }
+
+    for(let i=0;i<A.length;i++){
+        if(A[i] !== i+1){
+            return i+1;
+        }
+    }
+    return A.length+1;
+}
+
