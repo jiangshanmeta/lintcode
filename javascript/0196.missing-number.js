29d5a4060330c9a1a2ed01e0584179fe56f36a7c
jiangshanmeta <540118044@qq.com>
2019-10-23 00:35:55 +0800

    lintcode 196

---
+++ javascript/0196.missing-number.js
@@ -0,0 +1,27 @@
+/**
+ * @param nums: An array of integers
+ * @return: An integer
+ */
+const findMissing = function (nums) {
+    let index = 0;
+    while(index<nums.length){
+        if(nums[index] === index){
+            index++;
+            continue;
+        }
+        if(nums[nums[index]] !== nums[index]){
+            const tmp = nums[nums[index]];
+            nums[nums[index]] = nums[index];
+            nums[index] = tmp;
+        }else{
+            index++;
+        }
+    }
+    for(let i=0;i<nums.length;i++){
+        if(nums[i] !== i){
+            return i;
+        }
+    }
+    return nums.length;
+}
+
