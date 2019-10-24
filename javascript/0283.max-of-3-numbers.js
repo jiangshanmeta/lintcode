61aaa2249fca73abc2bba94c6276ffd1b0993c14
jiangshanmeta <540118044@qq.com>
2019-10-25 01:50:32 +0800

    lintcode 0283

---
+++ javascript/0283.max-of-3-numbers.js
@@ -0,0 +1,9 @@
+/**
+ * @param num1: An integer
+ * @param num2: An integer
+ * @param num3: An integer
+ * @return: an interger
+ */
+const maxOfThreeNumbers = function (num1, num2, num3) {
+    return Math.max(num1,num2,num3);
+}
