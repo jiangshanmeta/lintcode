718be0ff00555d7460d91cb527eecd26ce31a505
jiangshanmeta <540118044@qq.com>
2019-10-26 00:43:28 +0800

    lintcode 0366

---
+++ javascript/0366.fibonacci.js
@@ -0,0 +1,20 @@
+/**
+ * @param n: an integer
+ * @return: an ineger f(n)
+ */
+const fibonacci = function (n) {
+    n--;
+    if(n === 0){
+        return 0;
+    }else if(n === 1){
+        return 1;
+    }
+    let a = 0;
+    let b = 1;
+    let count = 1;
+    while(count<n){
+        count++;
+        [a,b] = [b,a+b];
+    }
+    return b;
+}
