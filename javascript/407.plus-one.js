7d5ab2db1ca9d1ef21fc3af707fe6a89df97f754
jiangshanmeta <540118044@qq.com>
2019-11-01 00:10:35 +0800

    lintcode 407

---
+++ javascript/407.plus-one.js
@@ -0,0 +1,17 @@
+/**
+ * @param digits: a number represented as an array of digits
+ * @return: the result
+ */
+const plusOne = function (digits) {
+    let carry = 1;
+    for(let i=digits.length-1;i>-1;i--){
+        const sum = digits[i]+carry;
+        const digit = sum%10;
+        carry = (sum-digit)/10;
+        digits[i] = digit;
+    }
+    carry && digits.unshift(carry);
+
+    return digits;
+}
+
