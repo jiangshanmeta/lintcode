20433a91da50d409317ee03fc7d24e9a39af191a
jiangshanmeta <540118044@qq.com>
2019-11-02 23:49:03 +0800

    lintcode 415

---
+++ javascript/0415.valid-palindrome.js
@@ -0,0 +1,28 @@
+/**
+ * @param s: A string
+ * @return: Whether the string is a valid palindrome
+ */
+const isPalindrome = function (s) {
+    s = s.toLowerCase();
+    let left = 0;
+    let right = s.length-1;
+    while(left<right){
+        while(left<right && !isConsider(s.charCodeAt(left))){
+            left++;
+        }
+        while(left<right && !isConsider(s.charCodeAt(right))){
+            right--;
+        }
+        if(left<right && s[left] !== s[right]){
+            return false;
+        }
+        left++;
+        right--;
+    }
+    return true;
+}
+
+function isConsider(code){
+    return (code>47 && code<58) || (code>96 && code<123);
+}
+
