ce0e6be91817c23e7e1e9409dc14cb5b7884495e
jiangshanmeta <540118044@qq.com>
2019-11-03 00:01:53 +0800

    lintcode 423

---
+++ javascript/0423.valid-parentheses.js
@@ -0,0 +1,27 @@
+/**
+ * @param s: A string
+ * @return: whether the string is a valid parentheses
+ */
+const isValidParentheses = function (s) {
+    const stack = [];
+    const left = {
+        '[':true,
+        '{':true,
+        '(':true,
+    }
+    const right2left = {
+        '}':'{',
+        ']':'[',
+        ')':'(',
+    }
+    for(let i=0;i<s.length;i++){
+        if(left[s[i]]){
+            stack.push(s[i]);
+        }else if(stack.length && stack[stack.length-1] === right2left[s[i]]){
+            stack.pop();
+        }else{
+            return false;
+        }
+    }
+    return stack.length === 0;
+}
