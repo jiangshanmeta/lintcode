f1d40c245ad3ad3cbe4be92f3ba0e263aab484f7
jiangshanmeta <540118044@qq.com>
2019-11-04 22:01:36 +0800

    lintcode 451

---
+++ javascript/0451.swap-nodes-in-pairs.js
@@ -0,0 +1,23 @@
+/**
+ * @param head: a ListNode
+ * @return: a ListNode
+ */
+const swapPairs = function (head) {
+    const dummyHead = new ListNode(0);
+    let prev = dummyHead;
+    while(head){
+        const next = head.next;
+        head.next = prev.next;
+        prev.next = head;
+        if(next){
+            head = next.next;
+            next.next = prev.next;
+            prev.next = next;
+            prev = prev.next.next;
+        }else{
+            break;
+        }
+    }
+    return dummyHead.next;
+}
+
