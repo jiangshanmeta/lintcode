f9a90f0f0085e0f0a25f7b79abd7e571cab16710
jiangshanmeta <540118044@qq.com>
2019-11-06 01:47:54 +0800

    lintcode 453

---
+++ javascript/0453.flatten-binary-tree-to-linked-list.js
@@ -0,0 +1,22 @@
+/**
+ * @param root: a TreeNode, the root of the binary tree
+ * @return: 
+ */
+const flatten = function (root) {
+    if(!root){
+        return root;
+    }
+    root.right = flatten(root.right);
+    if(root.left){
+        let left = flatten(root.left);
+        const right = root.right;
+        root.right = left;
+        while(left.right){
+            left = left.right;
+        }
+        left.right = right;
+        root.left = null;
+    }
+
+    return root;
+}
