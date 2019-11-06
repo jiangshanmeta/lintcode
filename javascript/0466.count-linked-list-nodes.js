e45152f33aef8f619d77ce6801077c185461a77a
jiangshanmeta <540118044@qq.com>
2019-11-07 00:14:08 +0800

    lintcode 466

---
+++ javascript/0466.count-linked-list-nodes.js
@@ -0,0 +1,12 @@
+/**
+ * @param head: the first node of linked list.
+ * @return: An integer
+ */
+const countNodes = function (head) {
+    let count = 0;
+    while(head){
+        count++;
+        head = head.next;
+    }
+    return count;
+}
