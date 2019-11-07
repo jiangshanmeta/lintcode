e985831a181c50653524a0595bf52b60dd13d84c
jiangshanmeta <540118044@qq.com>
2019-11-08 00:03:33 +0800

    lintcode 480

---
+++ javascript/0480.binary-tree-paths.js
@@ -0,0 +1,19 @@
+/**
+ * @param root: the root of the binary tree
+ * @return: all root-to-leaf paths
+ */
+const binaryTreePaths = function (root) {
+    const result = [];
+    function dfs(node,result,path){
+        path.push(node.val);
+        if(!node.left && !node.right){
+            result.push(path.join('->'));
+        }else{
+            node.left && dfs(node.left,result,path);
+            node.right && dfs(node.right,result,path);
+        }
+        path.pop();
+    }
+    root && dfs(root,result,[]);
+    return result;
+}
