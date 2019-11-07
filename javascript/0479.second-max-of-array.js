ef4c1b2d8bbe544b5af5488ad6f862af26ce778b
jiangshanmeta <540118044@qq.com>
2019-11-08 00:03:25 +0800

    lintcode 479

---
+++ javascript/0479.second-max-of-array.js
@@ -0,0 +1,15 @@
+/**
+ * @param nums: An integer array
+ * @return: The second max number in the array.
+ */
+const secondMax = function (nums) {
+    let first = Math.max(nums[0],nums[1]);
+    let second = Math.min(nums[0],nums[1]);
+    for(let i=2;i<nums.length;i++){
+        if(nums[i]>=first){
+            second = first;
+            first = nums[i]
+        }
+    }
+    return second;
+}
