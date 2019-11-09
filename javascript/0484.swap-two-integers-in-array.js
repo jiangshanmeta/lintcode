e0ac32a2ca674f07c931b8d200131c6c27df0b93
jiangshanmeta <540118044@qq.com>
2019-11-10 01:32:31 +0800

    lintcode 484

---
+++ javascript/0484.swap-two-integers-in-array.js
@@ -0,0 +1,11 @@
+/**
+ * @param A: An integer array
+ * @param index1: the first index
+ * @param index2: the second index
+ * @return: 
+ */
+const swapIntegers = function (A, index1, index2) {
+    const tmp = A[index1];
+    A[index1] = A[index2];
+    A[index2] = tmp;
+}
