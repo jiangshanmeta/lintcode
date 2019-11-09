060d096868c96341cb7af469810e4dc9a4ee5dbd
jiangshanmeta <540118044@qq.com>
2019-11-10 01:32:39 +0800

    lintcode 486

---
+++ javascript/0486.merge-k-sorted-arrays.js
@@ -0,0 +1,44 @@
+/**
+ * @param arrays: k sorted integer arrays
+ * @return: a sorted array
+ */
+const mergekSortedArrays = function (arrays) {
+    if(arrays.length === 0){
+        return [];
+    }
+    return mergeHelper(arrays,0,arrays.length-1);
+}
+
+function mergeHelper(arrays,start,end){
+    if(start === end){
+        return arrays[start];
+    }else if(start+1 === end){
+        return merge2(arrays[start],arrays[end]);
+    }else{
+        const mid = (start+end)>>1;
+        return merge2(
+            mergeHelper(arrays,start,mid),
+            mergeHelper(arrays,mid+1,end)
+        );
+    }
+}
+
+function merge2(arr1,arr2){
+    const result = [];
+    let index1 = 0;
+    let index2 = 0;
+    while(index1<arr1.length && index2<arr2.length){
+        if(arr1[index1]<arr2[index2]){
+            result.push(arr1[index1++]);
+        }else{
+            result.push(arr2[index2++]);
+        }
+    }
+    while(index1<arr1.length){
+        result.push(arr1[index1++]);
+    }
+    while(index2<arr2.length){
+        result.push(arr2[index2++]);
+    }
+    return result;
+}
