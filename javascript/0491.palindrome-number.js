4cb97154fe1b7cc6e900b80d6d505a50be578cee
jiangshanmeta <540118044@qq.com>
2019-11-14 00:56:42 +0800

    lintcode 491

---
+++ javascript/0491.palindrome-number.js
@@ -0,0 +1,18 @@
+/**
+ * @param num: a positive number
+ * @return: true if it's a palindrome or false
+ */
+const isPalindrome = function (num) {
+    num = ''+num;
+    let left = 0;
+    let right = num.length-1;
+    while(left<right){
+        if(num[left] !== num[right]){
+            return false;
+        }
+        left++;
+        right--;
+    }
+
+    return true;
+}
