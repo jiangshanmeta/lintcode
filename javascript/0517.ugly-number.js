0796d99a372676ac9a7203d391e3d5eee86b40b3
jiangshanmeta <540118044@qq.com>
2019-11-17 14:12:36 +0800

    lintcode 517

---
+++ javascript/0517.ugly-number.js
@@ -0,0 +1,20 @@
+/**
+ * @param num: An integer
+ * @return: true if num is an ugly number or false
+ */
+const isUgly = function (num) {
+    if(num<1){
+        return false;
+    }
+    while (num%2 === 0){
+        num /= 2;
+    }
+    while (num%3 === 0){
+        num /= 3;
+    }
+    while (num%5 === 0){
+        num /= 5;
+    }
+    return num === 1;
+}
+
