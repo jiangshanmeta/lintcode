8d6da5517c4397712127ee9e404fca41d26ccbda
jiangshanmeta <540118044@qq.com>
2019-11-18 13:01:45 +0800

    lintcode 539

---
+++ javascript/0539.move-zeroes.js
@@ -0,0 +1,24 @@
+/**
+ * @param nums: an integer array
+ * @return: 
+ */
+const moveZeroes = function (nums) {
+    let zeroIndex = 0;
+    let unzeroIndex = 0;
+    while(unzeroIndex<nums.length){
+        while(unzeroIndex<nums.length && nums[unzeroIndex] === 0){
+            unzeroIndex++;
+        }
+        while(zeroIndex<unzeroIndex && nums[zeroIndex] !== 0){
+            zeroIndex++;
+        }
+        if(unzeroIndex<nums.length && zeroIndex<unzeroIndex){
+            nums[zeroIndex] = nums[unzeroIndex];
+            nums[unzeroIndex] = 0;
+        }
+        zeroIndex++;
+        unzeroIndex++;
+
+    }
+}
+
