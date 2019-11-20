9119d21fc78a58cdc3abffe4c670cc3e0d26685d
jiangshanmeta <540118044@qq.com>
2019-11-21 00:58:57 +0800

    lintcode 585

---
+++ javascript/0585.maximum-number-in-mountain-sequence.js
@@ -0,0 +1,22 @@
+/**
+ * @param nums: a mountain sequence which increase firstly and then decrease
+ * @return: then mountain top
+ */
+const mountainSequence = function (nums) {
+    let low = 0;
+    let high = nums.length-1;
+    while(low<=high){
+        const mid = (low+high) >> 1;
+        const greaterLeft = mid >0 ? nums[mid]-nums[mid-1]>0 :true;
+        const greaterRight = mid<nums.length-1? nums[mid]-nums[mid+1]>0 :true;
+        if(greaterLeft && greaterRight){
+            return nums[mid];
+        }else if(greaterLeft){
+            low = mid+1;
+        }else{
+            high = mid-1;
+        }
+    }
+
+}
+
