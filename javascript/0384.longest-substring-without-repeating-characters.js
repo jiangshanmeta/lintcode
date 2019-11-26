0c154612d5e823eca01804d759586526a7fdd913
jiangshanmeta <540118044@qq.com>
2019-11-26 21:54:04 +0800

    lintcode 384

---
+++ javascript/0384.longest-substring-without-repeating-characters.js
@@ -0,0 +1,21 @@
+/**
+ * @param s: a string
+ * @return: an integer
+ */
+const lengthOfLongestSubstring = function (s) {
+    if(!s){
+        return 0;
+    }
+    const charIndexMap = [];
+    charIndexMap[s[0]] = 0;
+    let startIndex = 0;
+    let result = 1;
+    for(let i=1;i<s.length;i++){
+        if(charIndexMap[s[i]] !== undefined && charIndexMap[s[i]]>=startIndex){
+            startIndex = charIndexMap[s[i]]+1;
+        }
+        charIndexMap[s[i]] = i;
+        result = Math.max(result,i-startIndex+1);
+    }
+    return result
+}
