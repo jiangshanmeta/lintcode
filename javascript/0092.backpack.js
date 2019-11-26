e6d8343bd54e000fc1488ee88dfbe6810fc291fe
jiangshanmeta <540118044@qq.com>
2019-11-26 21:53:56 +0800

    lintcode 92

---
+++ javascript/0092.backpack.js
@@ -0,0 +1,26 @@
+/**
+ * @param m: An integer m denotes the size of a backpack
+ * @param A: Given n items with size A[i]
+ * @return: The maximum size
+ */
+const backPack = function (m, A) {
+    const dp = new Array(m+1).fill(false);
+    dp[0] = true;
+    for(let i=0;i<A.length;i++){
+        for(let j=m;j>-1;j--){
+            if(dp[j]){
+                continue;
+            }
+            if(j-A[i]<0){
+                break;
+            }
+            dp[j] = dp[j-A[i]];
+        }
+    }
+    for(let j=m;j>-1;j--){
+        if(dp[j]){
+            return j;
+        }
+    }
+}
+
