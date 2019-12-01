89ae88e48c9e15fad6948ac243a5982d0464cad3
jiangshanmeta <540118044@qq.com>
2019-12-01 21:35:10 +0800

    lintcode 402

---
+++ javascript/0402.continuous-subarray-sum.js
@@ -0,0 +1,25 @@
+/**
+ * @param A: An integer array
+ * @return: A list of integers includes the index of the first number and the index of the last number
+ */
+const continuousSubarraySum = function (A) {
+    let max = A[0];
+    let dp = A[0];
+    let result = [0,0];
+    let startIndex = 0;
+    for(let i=1;i<A.length;i++){
+        if(dp<0){
+            startIndex = i;
+            dp = A[i];
+        }else{
+            dp += A[i];
+        }
+        if(dp>max){
+            max = dp;
+            result[0] = startIndex;
+            result[1] = i;
+        }
+        
+    }
+    return result;
+}
