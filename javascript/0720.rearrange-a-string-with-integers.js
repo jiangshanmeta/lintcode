1e46c3edfd8ad79118e0850020c304988467b87a
jiangshanmeta <540118044@qq.com>
2019-12-07 00:12:12 +0800

    lintcode 720

---
+++ javascript/0720.rearrange-a-string-with-integers.js
@@ -0,0 +1,20 @@
+/**
+ * @param str: a string containing uppercase alphabets and integer digits
+ * @return: the alphabets in the order followed by the sum of digits
+ */
+const rearrange = function (str) {
+    let sum = 0;
+    let hasDigit = false;
+    let result = [];
+    for(let i=0;i<str.length;i++){
+        const code = str.charCodeAt(i);
+        if(code>64){
+            result.push(str[i]);
+        }else{
+            sum += (+str[i]);
+            hasDigit = true;
+        }
+    }
+
+    return `${result.sort().join('')}${hasDigit?sum:''}`;
+}
