c24178bd1d3af7301dcf8d907557df68d2802972
jiangshanmeta <540118044@qq.com>
2019-12-09 08:13:09 +0800

    lintcode 730

---
+++ javascript/0730.sum-of-all-subsets.js
@@ -0,0 +1,12 @@
+/**
+ * @param n: the given number
+ * @return: Sum of elements in subsets
+ */
+const subSum = function (n) {
+    let result = 0;
+    for(let i=1;i<n+1;i++){
+        result += i*Math.pow(2,n-1);
+    }
+    return result;
+}
+
