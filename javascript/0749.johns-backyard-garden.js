2f6b0e841fde5e822cf261ee10dd7c91a35916a0
jiangshanmeta <540118044@qq.com>
2019-12-09 08:13:28 +0800

    lintcode 749

---
+++ javascript/0749.johns-backyard-garden.js
@@ -0,0 +1,15 @@
+/**
+ * @param x: the wall's height
+ * @return: YES or NO
+ */
+const isBuild = function (x) {
+    const dp = new Array(x+1);
+    dp[0] = true;
+    dp[3] = true;
+    dp[7] = true;
+    for(let i=8;i<x+1;i++){
+        dp[i] = dp[i-3] || dp[i-7];
+    }
+    return dp[x]?"YES":"NO";
+}
+
