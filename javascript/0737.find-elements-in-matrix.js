62f4ea0087a3d966f20e7b68a45c6d3c8eb6cb1d
jiangshanmeta <540118044@qq.com>
2019-12-09 08:13:19 +0800

    lintcode 737

---
+++ javascript/0737.find-elements-in-matrix.js
@@ -0,0 +1,25 @@
+/**
+ * @param Matrix: the input
+ * @return: the element which appears every row
+ */
+const FindElements = function (Matrix) {
+    const N = Matrix.length;
+    const M = Matrix[0].length;
+    const map = {};
+    for(let i=0;i<N;i++){
+        const map2 = {};
+        for(let j=0;j<M;j++){
+            const node = Matrix[i][j];
+            if(map2[node]){
+                continue;
+            }
+            map2[node] = true;
+            map[node] = (map[node] || 0)+1;
+            if(map[node] === N){
+                return node
+            }
+        }
+    }
+
+}
+
