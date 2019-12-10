a878f17df1474f4fac06b5711b1ee81107cc786f
jiangshanmeta <540118044@qq.com>
2019-12-11 00:07:13 +0800

    lintcode 768

---
+++ javascript/0768.yang-hui-triangle.js
@@ -0,0 +1,20 @@
+/**
+ * @param n: a Integer
+ * @return: the first n-line Yang Hui's triangle
+ */
+const calcYangHuisTriangle = function (n) {
+    if(n === 0){
+        return [];
+    }
+    const result = [[1]];
+    for(let i=1;i<n;i++){
+        const lastRow = result[result.length-1];
+        const newRow = [1];
+        for(let j=0;j<i-1;j++){
+            newRow.push(lastRow[j]+lastRow[j+1]);
+        }
+        newRow.push(1);
+        result.push(newRow);
+    }
+    return result;
+}
