85b5ace0796c4783f32ba621dd2bdb5f17a535f1
jiangshanmeta <540118044@qq.com>
2019-12-11 00:07:05 +0800

    lintcode 767

---
+++ javascript/0767.reverse-array.js
@@ -0,0 +1,14 @@
+/**
+ * @param nums: a integer array
+ * @return: 
+ */
+const reverseArray = function (nums) {
+    let left = 0;
+    let right = nums.length-1;
+    while(left<right){
+        const tmp = nums[left]
+        nums[left++] = nums[right];
+        nums[right--] = tmp;
+    }
+}
+
