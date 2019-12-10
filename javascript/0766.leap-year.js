0a9081680c83e98c0f27863f335cc0f93feff9b1
jiangshanmeta <540118044@qq.com>
2019-12-11 00:06:57 +0800

    lintcode 766

---
+++ javascript/0766.leap-year.js
@@ -0,0 +1,11 @@
+/**
+ * @param n: a number represent year
+ * @return: whether year n is a leap year.
+ */
+const isLeapYear = function (n) {
+    if( (n%4 === 0 && n%100 !== 0) || n%400 === 0 ){
+        return true;
+    }
+    return false;
+}
+
