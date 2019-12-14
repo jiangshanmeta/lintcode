37bdd5f7cbba84f13e8cf4c291758f1f8f1e5101
jiangshanmeta <540118044@qq.com>
2019-12-14 22:05:44 +0800

    lintcode 777

---
+++ javascript/0777.valid-perfect-square.js
@@ -0,0 +1,24 @@
+/**
+ * @param num: a positive integer
+ * @return: if num is a perfect square else False
+ */
+const isPerfectSquare = function (num) {
+    if(num === 1){
+        return true;
+    }
+    // 就是开根号
+    let low = 1;
+    let high = num-1;
+    while(low<=high){
+        const mid = low + ((high-low) >> 1);
+        const square = mid*mid;
+        if(square===num){
+            return true;
+        }else if(square>num){
+            high = mid-1;
+        }else{
+            low = mid+1;
+        }
+    }
+    return false;
+}
