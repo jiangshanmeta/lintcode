07db121fb85f504876a70ff9a23deecbfb3f9fbb
jiangshanmeta <540118044@qq.com>
2019-12-15 00:02:16 +0800

    lintcode 823

---
+++ javascript/0823.input-stream.js
@@ -0,0 +1,33 @@
+/**
+ * @param inputA: Input stream A
+ * @param inputB: Input stream B
+ * @return: The answer
+ */
+const inputStream = function (inputA, inputB) {
+    const stackA = [];
+    const stackB = [];
+    for(let i=0;i<inputA.length;i++){
+        if(inputA[i] === '<'){
+            stackA.pop();
+        }else{
+            stackA.push(inputA[i]);
+        }
+    }
+    for(let i=0;i<inputB.length;i++){
+        if(inputB[i] === '<'){
+            stackB.pop();
+        }else{
+            stackB.push(inputB[i]);
+        }
+    }
+    if(stackA.length !== stackB.length){
+        return "NO";
+    }
+    for(let i=0;i<stackA.length;i++){
+        if(stackA[i] !== stackB[i]){
+            return "NO";
+        }
+    }
+    return "YES";
+}
+
