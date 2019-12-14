47106d2a61231e804a5a1454d4c11a4ea5b05acf
jiangshanmeta <540118044@qq.com>
2019-12-15 00:02:01 +0800

    lintcode 813

---
+++ javascript/0813.find-anagram-mappings.js
@@ -0,0 +1,18 @@
+/**
+ * @param A: lists A
+ * @param B: lists B
+ * @return: the index mapping
+ */
+const anagramMappings = function (A, B) {
+    // 这题OJ有问题，AC了，就是hash而已
+    const bMap = [];
+    for(let i=0;i<B.length;i++){
+        bMap[B[i]] = i;
+    }
+    const result = [];
+    for(let i=0;i<A.length;i++){
+        result.push(bMap[A[i]])
+    }
+    return result;
+}
+
