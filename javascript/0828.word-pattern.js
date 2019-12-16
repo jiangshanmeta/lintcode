4c128ac5bcec4f5f6aef3fd831fea705d7d2af67
jiangshanmeta <540118044@qq.com>
2019-12-16 13:22:44 +0800

    lintcode 828

---
+++ javascript/0828.word-pattern.js
@@ -0,0 +1,20 @@
+/**
+ * @param pattern: a string, denote pattern string
+ * @param teststr: a string, denote matching string
+ * @return: an boolean, denote whether the pattern string and the matching string match or not
+ */
+const wordPattern = function (pattern, teststr) {
+    const a2b = {};
+    const b2a = {};
+    teststr = teststr.split(' ');
+    for(let i=0;i<pattern.length;i++){
+        if(a2b[pattern[i]] === undefined && b2a[teststr[i]] === undefined){
+            a2b[pattern[i]] = teststr[i];
+            b2a[teststr[i]] = pattern[i];
+        }else if(a2b[pattern[i]] !== teststr[i] || b2a[teststr[i]] !== pattern[i]){
+            return false;
+        }
+    }
+    return true;
+}
+
