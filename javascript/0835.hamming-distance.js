b7f2cedb35a90c13b49eb026f15260ab6647a6f1
jiangshanmeta <540118044@qq.com>
2019-12-18 01:41:35 +0800

    lintcode 835

---
+++ javascript/0835.hamming-distance.js
@@ -0,0 +1,18 @@
+/**
+ * @param x: an integer
+ * @param y: an integer
+ * @return: return an integer, denote the Hamming Distance between two integers
+ */
+const hammingDistance = function (x, y) {
+    let result = 0;
+    while(x || y){
+        const digit1 = x&1;
+        const digit2 = y&1;
+        if(digit1 !== digit2){
+            result++;
+        }
+        x >>= 1;
+        y >>= 1;
+    }
+    return result;
+}
