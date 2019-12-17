3e6bd44fce6479db10fc7f4bb69ecf445a5fe7d2
jiangshanmeta <540118044@qq.com>
2019-12-18 01:41:57 +0800

    lintcode 845

---
+++ javascript/0845.greatest-common-divisor.js
@@ -0,0 +1,16 @@
+/**
+ * @param a: the given number
+ * @param b: another number
+ * @return: the greatest common divisor of two numbers
+ */
+const gcd = function (a, b) {
+    if(a<b){
+        [a,b] = [b,a]
+    }
+    while(b !== 0){
+        const tmp = a%b;
+        a = b;
+        b = tmp;
+    }
+    return a
+}
