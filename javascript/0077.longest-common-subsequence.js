fbb60f329f535d12f0a210ac45a3d7648690d15b
jiangshanmeta <540118044@qq.com>
2019-12-22 00:25:07 +0800

    lintcode 0077

---
+++ javascript/0077.longest-common-subsequence.js
@@ -0,0 +1,23 @@
+/**
+ * @param A: A string
+ * @param B: A string
+ * @return: The length of longest common subsequence of A and B
+ */
+const longestCommonSubsequence = function (A, B) {
+    let dp = new Array(B.length+1).fill(0);
+    for(let i=1;i<=A.length;i++){
+        const newDp = new Array(B.length+1);
+        newDp[0] = 0;
+        for(let j=1;j<=B.length;j++){
+            newDp[j] = Math.max(
+                dp[j],
+                A[i-1] === B[j-1]?dp[j-1]+1:0,
+                newDp[j-1]
+            );
+        }
+        dp = newDp;
+    }
+
+    return Math.max(...dp);
+}
+
