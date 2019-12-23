054fcf08e18edc4dd7df95772ea84c71327dc323
jiangshanmeta <540118044@qq.com>
2019-12-23 14:57:10 +0800

    lintcode 0149

---
+++ javascript/0149.best-time-to-buy-and-sell-stock.js
@@ -0,0 +1,13 @@
+/**
+ * @param prices: Given an integer array
+ * @return: Maximum profit
+ */
+const maxProfit = function (prices) {
+    let result = 0;
+    let prevMin = Infinity;
+    for(let i=0;i<prices.length;i++){
+        result = Math.max(result,prices[i]-prevMin);
+        prevMin = Math.min(prevMin,prices[i])
+    }
+    return result;
+}
