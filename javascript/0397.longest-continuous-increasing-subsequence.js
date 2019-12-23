fcccc592c2941eaa4b7cfb382d5c1ccd01c3b24d
jiangshanmeta <540118044@qq.com>
2019-12-24 00:04:04 +0800

    lintcode 0397

---
+++ javascript/0397.longest-continuous-increasing-subsequence.js
@@ -0,0 +1,33 @@
+/**
+ * @param A: An array of Integer
+ * @return: an integer
+ */
+const longestIncreasingContinuousSubsequence = function (A) {
+    if(A.length === 0){
+        return 0;
+    }
+    let result = 1;
+    let lastVal = A[0];
+    let length = 1;
+    for(let i=1;i<A.length;i++){
+        if(A[i]>lastVal){
+            length++;
+        }else{
+            length = 1;
+        }
+        lastVal = A[i];
+        result = Math.max(result,length);
+    }
+    lastVal = A[A.length-1];
+    length = 1;
+    for(let i=A.length-2;i>-1;i--){
+        if(A[i]>lastVal){
+            length++;
+        }else{
+            length = 1;
+        }
+        lastVal = A[i];
+        result = Math.max(result,length);
+    }
+    return result;
+}
