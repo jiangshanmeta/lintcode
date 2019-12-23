2f134b563259db14c7daa4ad31b773f90f7ddeb2
jiangshanmeta <540118044@qq.com>
2019-12-23 14:57:03 +0800

    lintcode 0079

---
+++ javascript/0079.longest-common-substring.js
@@ -0,0 +1,23 @@
+/**
+ * @param A: A string
+ * @param B: A string
+ * @return: the length of the longest common substring.
+ */
+const longestCommonSubstring = function (A, B) {
+    // dp[i][j] 以A[i] B[j] 为结尾可以构成的最大的子串长度
+    // 空间压缩成了一维的
+    const dp = new Array(B.length+1).fill(0);
+    let result = 0;
+    for(let i=1;i<=A.length;i++){
+        for(let j=B.length;j>0;j--){
+            if(A[i-1] === B[j-1]){
+                dp[j] = dp[j-1]+1;
+                result = Math.max(dp[j],result);
+            }else{
+                dp[j] = 0;
+            }
+        }
+    }
+    return result;
+}
+
