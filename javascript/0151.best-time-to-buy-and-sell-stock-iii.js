de8f2535d61ee96521c24f2d0502687f844e6c01
jiangshanmeta <540118044@qq.com>
2019-12-24 00:03:49 +0800

    lintcode 0151

---
+++ javascript/0151.best-time-to-buy-and-sell-stock-iii.js
@@ -0,0 +1,15 @@
+/**
+ * @param prices: Given an integer array
+ * @return: Maximum profit
+ */
+const maxProfit = function (prices) {
+    const dp = new Array(5).fill(-Infinity);
+    dp[0] = 0;
+    for(let i=0;i<prices.length;i++){
+        for(let j=4;j>0;j--){
+            dp[j] = Math.max(dp[j],dp[j-1]+prices[i]*(j&1?-1:1) );
+        }
+    }
+    return Math.max(...dp);
+}
+
