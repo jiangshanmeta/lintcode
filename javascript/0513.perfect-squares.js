c037d915afbbfcb4b029ddde4d64b79c153640a2
jiangshanmeta <540118044@qq.com>
2019-12-25 00:14:01 +0800

    lintcode 0513

---
+++ javascript/0513.perfect-squares.js
@@ -0,0 +1,22 @@
+/**
+ * @param n: a positive integer
+ * @return: An integer
+ */
+const numSquares = function (n) {
+    const candidate = [];
+    const max = Math.floor(Math.sqrt(n));
+    for(let i=1;i<=max;i++){
+        candidate.push(i*i);
+    }
+    const dp = new Array(n+1).fill(Infinity);
+    dp[0] = 0;
+    for(let i=1;i<dp.length;i++){
+        for(let j=0;j<candidate.length;j++){
+            if(i<candidate[j]){
+                break;
+            }
+            dp[i] = Math.min(dp[i],dp[i-candidate[j]]+1)
+        }
+    }
+    return dp[n];
+}
