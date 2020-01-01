fefdd0d7fecfcc5b8edeb1e309c967b35a35c253
jiangshanmeta <540118044@qq.com>
2020-01-01 15:18:15 +0800

    lintcode 562

---
+++ javascript/0562.backpack-iv.js
@@ -0,0 +1,16 @@
+/**
+ * @param nums: an integer array and all positive numbers, no duplicates
+ * @param target: An integer
+ * @return: An integer
+ */
+const backPackIV = function (nums, target) {
+    const dp = new Array(target+1).fill(0);
+    dp[0] = 1;
+    for(let i=0;i<nums.length;i++){
+        // 因为一个物品可以无限次使用
+        for(let j=nums[i];j<dp.length;j++){
+            dp[j] += dp[j-nums[i]];
+        }
+    }
+    return dp[target]
+}
