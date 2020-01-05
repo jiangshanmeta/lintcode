cd352d5c137891de1d06d842c746a4a984c97dc5
jiangshanmeta <540118044@qq.com>
2020-01-05 14:35:39 +0800

    lintcode 761

---
+++ javascript/0761.smallest-subset.js
@@ -0,0 +1,20 @@
+/**
+ * @param arr:  an array of non-negative integers
+ * @return: minimum number of elements
+ */
+const minElements = function (arr) {
+    arr.sort((a,b)=>b-a);
+    let sum = 0;
+    let rest = 0;
+    for(let i=0;i<arr.length;i++){
+        rest += arr[i];
+    }
+    for(let i=0;i<arr.length;i++){
+        sum += arr[i];
+        rest -= arr[i];
+        if(sum>rest){
+            return i+1;
+        }
+    }
+    return 0;
+}
