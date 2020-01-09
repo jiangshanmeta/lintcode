b946fdfd2ce15c648d1dd40d3f2caa9a5140e4c1
jiangshanmeta <540118044@qq.com>
2020-01-09 12:43:29 +0800

    lintcode 846

---
+++ javascript/0846.multi-keyword-sort.js
@@ -0,0 +1,13 @@
+/**
+ * @param array: the input array
+ * @return: the sorted array
+ */
+const multiSort = function (array) {
+    return array.sort((a,b)=>{
+        if(a[1] !== b[1]){
+            return b[1]-a[1];
+        }
+        return a[0]-b[0];
+    });
+}
+
