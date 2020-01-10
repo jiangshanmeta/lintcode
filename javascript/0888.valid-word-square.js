a921f4a116a441dd7d2665282841c93dac4756b3
jiangshanmeta <540118044@qq.com>
2020-01-10 12:45:34 +0800

    lintcode 888

---
+++ javascript/0888.valid-word-square.js
@@ -0,0 +1,18 @@
+/**
+ * @param words: a list of string
+ * @return: a boolean
+ */
+const validWordSquare = function (words) {
+    for(let i=0;i<words.length;i++){
+        for(let j=0;j<words[i].length;j++){
+            if(j>=words.length || i>=words[i].length){
+                break;
+            }
+            if(words[i][j] !== words[j][i]){
+                return false;
+            }
+        }
+    }
+    return true;
+}
+
