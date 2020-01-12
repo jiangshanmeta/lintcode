ff70e496836ff3cc396187d2d6f9e7c5219a9dd7
jiangshanmeta <540118044@qq.com>
2020-01-12 12:08:34 +0800

    lintcode 911

---
+++ javascript/0911.maximum-size-subarray-sum-equals-k.js
@@ -0,0 +1,24 @@
+/**
+ * @param nums: an array
+ * @param k: a target value
+ * @return: the maximum length of a subarray that sums to k
+ */
+const maxSubArrayLen = function (nums, k) {
+    // hash[key] 前缀和为key的数组最早出现的索引
+    const hash = {};
+    hash[0] = -1;
+    let sum = 0;
+    let result = 0;
+    for(let i=0;i<nums.length;i++){
+        sum += nums[i];
+        if(hash[sum-k] !== undefined){
+            result = Math.max(result,i-hash[sum-k]);
+        }
+        if(hash[sum] === undefined){
+            hash[sum] = i;
+        }
+        
+    }
+    return result;
+}
+
