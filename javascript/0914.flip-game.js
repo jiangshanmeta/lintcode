ed089f0907711be5b1751b26860e81114db80476
jiangshanmeta <540118044@qq.com>
2020-01-12 12:08:42 +0800

    lintcode 914

---
+++ javascript/0914.flip-game.js
@@ -0,0 +1,19 @@
+/**
+ * @param s: the given string
+ * @return: all the possible states of the string after one valid move
+ */
+const generatePossibleNextMoves = function (s) {
+    const list = s.split('');
+    const result = [];
+    for(let i=1;i<list.length;i++){
+        if(list[i] === '+' && list[i-1] === '+'){
+            list[i] = '-';
+            list[i-1] = '-';
+            result.push(list.join(''));
+            list[i] = '+';
+            list[i-1] = '+';
+        }
+    }
+    return result;
+}
+
