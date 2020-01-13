54a92af114e7245c9cb48c7a1ceb4f86dd86e3c1
jiangshanmeta <540118044@qq.com>
2020-01-13 17:23:19 +0800

    lintcode 927

---
+++ javascript/0927.reverse-words-in-a-string-ii.js
@@ -0,0 +1,29 @@
+/**
+ * @param str: a string
+ * @return: return a string
+ */
+const reverseWords = function (str) {
+    str = str.split('').reverse();
+    let index  = 0;
+    while(index<str.length){
+        while(index<str.length && str[index] === ' '){
+            index++;
+        }
+        const start = index;
+        while(index<str.length && str[index] !== ' '){
+            index++;
+        }
+        reverse(str,start,index-1);
+    }
+    return str.join('');
+}
+
+function reverse(sequence,left,right){
+    while(left<right){
+        const tmp = sequence[right];
+        sequence[right] = sequence[left];
+        sequence[left] = tmp;
+        left++;
+        right--;
+    }
+}
