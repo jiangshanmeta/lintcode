c1707234e5e4edf0b5935b21fd3696d6516b01b1
jiangshanmeta <540118044@qq.com>
2020-01-13 17:23:11 +0800

    lintcode 924

---
+++ javascript/0924.shortest-word-distance.js
@@ -0,0 +1,25 @@
+/**
+ * @param words: a list of words
+ * @param word1: a string
+ * @param word2: a string
+ * @return: the shortest distance between word1 and word2 in the list
+ */
+const shortestDistance = function (words, word1, word2) {
+    let index1 = -1;
+    let index2 = -1;
+    let result = words.length;
+    for(let i=0;i<words.length;i++){
+        if(words[i] === word1){
+            if(index2 !== -1){
+                result = Math.min(result,i-index2);
+            }
+            index1 = i;
+        }else if(words[i] === word2){
+            if(index1 !== -1){
+                result = Math.min(result,i-index1);
+            }
+            index2 = i;
+        }
+    }
+    return result;
+}
