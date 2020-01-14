915f39aaee4604024180ea259cceade55661770e
jiangshanmeta <540118044@qq.com>
2020-01-14 18:36:48 +0800

    lintcode 976

---
+++ javascript/0976.4sum-ii.js
@@ -0,0 +1,27 @@
+/**
+ * @param A: a list
+ * @param B: a list
+ * @param C: a list
+ * @param D: a list
+ * @return: how many tuples (i, j, k, l) there are such that A[i] + B[j] + C[k] + D[l] is zero
+ */
+const fourSumCount = function (A, B, C, D) {
+    const L = A.length;
+    const sumCountMap = {};
+    for(let i=0;i<L;i++){
+        for(let j=0;j<L;j++){
+            const sum = C[i]+D[j];
+            sumCountMap[sum] = (sumCountMap[sum] || 0)+1;
+        }
+    }
+    let result = 0;
+    for(let i=0;i<L;i++){
+        for(let j=0;j<L;j++){
+            const rest = -A[i]-B[j];
+            if(sumCountMap[rest] !== undefined){
+                result += sumCountMap[rest];
+            }
+        }
+    }
+    return result;
+}
