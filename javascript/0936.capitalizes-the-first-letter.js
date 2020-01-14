43dfcef6d1450be9ba4e2208974de7c53dd0b92c
jiangshanmeta <540118044@qq.com>
2020-01-14 18:36:19 +0800

    leetcode 936

---
+++ javascript/0936.capitalizes-the-first-letter.js
@@ -0,0 +1,21 @@
+/**
+ * @param s: a string
+ * @return: a string after capitalizes the first letter
+ */
+const capitalizesFirst = function (s) {
+    const result = [];
+    let lastIsSpace = true;
+    for(let i=0;i<s.length;i++){
+        if(s[i] === ' '){
+            lastIsSpace = true;
+            result.push(s[i]);
+        }else if(lastIsSpace){
+            lastIsSpace = false;
+            result.push(s[i].toUpperCase());
+        }else{
+            result.push(s[i]);
+        }
+    }
+
+    return result.join('');
+}
