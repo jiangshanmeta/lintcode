352ad045f5f580f0c1ba84684a195ef41b691889
jiangshanmeta <540118044@qq.com>
2020-01-16 11:21:09 +0800

    leetcode 977

---
+++ javascript/0977.base-7.js
@@ -0,0 +1,19 @@
+/**
+ * @param num: the given number
+ * @return: The base 7 string representation
+ */
+const convertToBase7 = function (num) {
+    if(num<0){
+        return `-${convertToBase7(-num)}`;
+    }else if(num === 0){
+        return "0";
+    }
+    const stack = [];
+    while(num>0){
+        const digit = num%7;
+        stack.push(digit);
+        num = (num-digit)/7;
+    }
+    stack.reverse();
+    return stack.join('');
+}
