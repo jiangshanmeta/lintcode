f71f8f4858578fe36f5ce344cb81dd3d32326e72
jiangshanmeta <540118044@qq.com>
2020-01-17 11:43:19 +0800

    lintcode 987

---
+++ javascript/0987.binary-number-with-alternating-bits.js
@@ -0,0 +1,11 @@
+/**
+ * @param n: a postive Integer
+ * @return: if two adjacent bits will always have different values
+ */
+const hasAlternatingBits = function (n) {
+    // 对于交替位的二进制数 右移 再和原数字做xor 得到的数字所有位均为1 即2^n -1
+    // 于是转换成了如何判断一个数是否是2的幂次
+    n = n^(n>>1)
+    return (n&(n+1)) === 0;
+}
+
