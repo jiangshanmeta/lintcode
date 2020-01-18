383b8513724cdfb996b141c1dfe5829233e1709c
jiangshanmeta <540118044@qq.com>
2020-01-18 14:53:13 +0800

    lintcode 995

---
+++ javascript/0995.best-time-to-buy-and-sell-stock-with-cooldown.js
@@ -0,0 +1,24 @@
+/**
+ * @param prices: a list of integers
+ * @return: return a integer
+ */
+const maxProfit = function (prices) {
+    if(prices.length === 0){
+        return 0;
+    }
+    
+    let buy = -prices[0];
+    let sell = 0;
+    let colldown = 0;
+    let result = 0;
+    for(let i=1;i<prices.length;i++){
+        const newBuy = Math.max(buy,colldown-prices[i]);
+        const newSell = Math.max(sell,buy+prices[i]);
+        const newColldown = Math.max(colldown,sell);
+        result = Math.max(newBuy,newSell,newColldown,result);
+        buy = newBuy;
+        sell = newSell;
+        colldown = newColldown;
+    }
+    return result;
+}
