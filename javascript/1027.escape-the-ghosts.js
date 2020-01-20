f61c149b32d527735f17b97695de570e96c8f62d
jiangshanmeta <540118044@qq.com>
2020-01-20 11:18:44 +0800

    lintcode 1027

---
+++ javascript/1027.escape-the-ghosts.js
@@ -0,0 +1,16 @@
+/**
+ * @param ghosts: a 2D integer array
+ * @param target: a integer array
+ * @return: return boolean
+ */
+const escapeGhosts = function (ghosts, target) {
+    const DIS = Math.abs(target[0]) + Math.abs(target[1]);
+    for(let i=0;i<ghosts.length;i++){
+        const [x,y] = ghosts[i];
+        const dis = Math.abs(target[0]-x)+Math.abs(target[1]-y);
+        if(dis<=DIS){
+            return false;
+        }
+    }
+    return true;
+}
