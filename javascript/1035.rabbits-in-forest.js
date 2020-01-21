b6e82b3dbfa479e53cba28aee4288418e4cf6ff0
jiangshanmeta <540118044@qq.com>
2020-01-21 13:55:27 +0800

    lintcode 1035

---
+++ javascript/1035.rabbits-in-forest.js
@@ -0,0 +1,17 @@
+/**
+ * @param answers: some subset of rabbits (possibly all of them) tell 
+ * @return: the minimum number of rabbits that could be in the forest.
+ */
+const numRabbits = function (answers) {
+    const map = {};
+    for(let i=0;i<answers.length;i++){
+        map[answers[i]] = (map[answers[i]] || 0)+1;
+    }
+    let result = 0;
+    const keys = Object.keys(map);
+    for(let i=0;i<keys.length;i++){
+        const answer = +keys[i];
+        result += (answer+1)*Math.ceil(map[answer]/(answer+1));
+    }
+    return result;
+}
