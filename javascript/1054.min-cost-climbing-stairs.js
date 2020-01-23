86143df78d55f42fbe53905e3e2e75ec441afa9f
jiangshanmeta <540118044@qq.com>
2020-01-23 15:55:10 +0800

    lintcode 1054

---
+++ javascript/1054.min-cost-climbing-stairs.js
@@ -0,0 +1,14 @@
+/**
+ * @param cost: an array
+ * @return: minimum cost to reach the top of the floor
+ */
+const minCostClimbingStairs = function (cost) {
+    if(cost.length<2){
+        return 0;
+    }
+    let [a,b] = cost;
+    for(let i=2;i<cost.length;i++){
+        [a,b] = [b,Math.min(a,b)+cost[i]]
+    }
+    return Math.min(a,b);
+}
