6d34b39e284f268cdfcf78844a2823720f636d5f
jiangshanmeta <540118044@qq.com>
2020-01-23 15:54:55 +0800

    lintcode 1042

---
+++ javascript/1042.toeplitz-matrix.js
@@ -0,0 +1,35 @@
+/**
+ * @param matrix: the given matrix
+ * @return: True if and only if the matrix is Toeplitz
+ */
+const isToeplitzMatrix = function (matrix) {
+    if(!matrix.length){
+        return true;
+    }
+    const M = matrix.length;
+    const N = matrix[0].length;
+    for(let i=0;i<M;i++){
+        const node = matrix[i][0];
+        let j = i+1;
+        let k = 1;
+        while(j<M && k<N){
+            if(matrix[j++][k++] !== node){
+                return false;
+            }
+        }
+    }
+
+    for(let i=1;i<N;i++){
+        const node = matrix[0][i];
+        let j = 1;
+        let k = i+1;
+        while(j<M && k<N){
+            if(matrix[j++][k++] !== node){
+                return false;
+            }
+        }
+    }
+
+    return true;
+}
+
