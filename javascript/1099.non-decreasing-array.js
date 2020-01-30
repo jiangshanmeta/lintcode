60b536fbaddcff58eea30e5ca39fec0f472efd32
jiangshanmeta <540118044@qq.com>
2020-01-30 16:22:16 +0800

    lintcode 1099

---
+++ javascript/1099.non-decreasing-array.js
@@ -0,0 +1,20 @@
+/**
+ * @param nums: an array
+ * @return: if it could become non-decreasing by modifying at most 1 element
+ */
+const checkPossibility = function (nums) {
+    let changed = false;
+    for(let i=1;i<nums.length;i++){
+        if(nums[i-1]>nums[i]){
+            if(changed){
+                return false;
+            }
+            changed = true;
+            if(i>1 && nums[i-2]>nums[i]){
+                nums[i] = nums[i-1];
+            }
+        }
+    }
+    return true;
+}
+
