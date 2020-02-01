0a1e6b6b8bb4921d10de8ba48aa002a0fd0d6f3e
jiangshanmeta <540118044@qq.com>
2020-02-01 15:32:18 +0800

    lintcode 1137

---
+++ javascript/1137.construct-string-from-binary-tree.js
@@ -0,0 +1,29 @@
+/**
+ * @param t: the root of tree
+ * @return: return a string
+ */
+const tree2str = function (t) {
+    const result = [];
+    preOrder(t,result);
+    return result.join('');
+}
+
+function preOrder(root,result){
+    if(!root){
+        return;
+    }
+    result.push(root.val);
+    if(!root.left && !root.right){
+        return;
+    }
+    result.push('(');
+    preOrder(root.left,result);
+    result.push(')');
+    
+    if(root.right){
+        result.push('(');
+        preOrder(root.right,result);
+        result.push(')');
+    }
+    
+}
