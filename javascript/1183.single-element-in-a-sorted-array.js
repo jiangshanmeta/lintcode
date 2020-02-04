dcb47d55d0c64e9f6827fe251576f3ed79c84082
jiangshanmeta <540118044@qq.com>
2020-02-05 02:15:13 +0800

    lintcode 1183

---
+++ javascript/1183.single-element-in-a-sorted-array.js
@@ -0,0 +1,29 @@
+/**
+ * @param nums: a list of integers
+ * @return: return a integer
+ */
+const singleNonDuplicate = function (nums) {
+    let low = 0;
+    let high = nums.length-1;
+    while(low<=high){
+        const mid = (low+high) >> 1;
+        if(mid%2 === 1){
+            if(nums[mid] === nums[mid-1]){
+                low = mid+1;
+            }else if(mid<nums.length-1 && nums[mid] === nums[mid+1]){
+                high = mid-1;
+            }else{
+                return nums[mid];
+            }
+        }else{
+            if(nums[mid] === nums[mid+1]){
+                low = mid+1;
+            }else if(mid>0 && nums[mid] === nums[mid-1]){
+                high = mid-1;
+            }else{
+                return nums[mid];
+            }
+        }
+    }
+}
+
