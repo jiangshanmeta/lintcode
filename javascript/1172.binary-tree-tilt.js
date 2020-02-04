7c0bb1aa004b822fca760441f1e5d2974579e90b
jiangshanmeta <540118044@qq.com>
2020-02-04 15:02:50 +0800

    lintcode 1172

---
+++ javascript/1172.binary-tree-tilt.js
@@ -0,0 +1,19 @@
+/**
+ * @param root: the root
+ * @return: the tilt of the whole tree
+ */
+const findTilt = function (root) {
+    let result = 0;
+    function postOrder(root){
+        if(!root){
+            return 0;
+        }
+        const left = postOrder(root.left);
+        const right = postOrder(root.right);
+        const current = Math.abs(left-right);
+        result += current;
+        return left+right+root.val;
+    }
+    postOrder(root);
+    return result;
+}
