9a032eebc084e9ff6edd6cbae7cf31fd7ff51bf7
jiangshanmeta <540118044@qq.com>
2020-02-04 15:03:00 +0800

    lintcode 1173

---
+++ javascript/1173.reverse-words-in-a-string-iii.js
@@ -0,0 +1,24 @@
+/**
+ * @param s: a string
+ * @return: reverse the order of characters in each word within a sentence while still preserving whitespace and initial word order
+ */
+const reverseWords = function (s) {
+    const result = [];
+    const stack = [];
+    for(let i=0;i<s.length;i++){
+        if(s[i] === ' '){
+            while(stack.length){
+                result.push(stack.pop());
+            }
+            result.push(s[i]);
+        }else{
+            stack.push(s[i]);
+        }
+    }
+
+    while(stack.length){
+        result.push(stack.pop());
+    }
+
+    return result.join('');
+}
