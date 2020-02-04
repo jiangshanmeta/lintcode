69c4a95eb0cd12fb5b58778756a361aea5ddc3a5
jiangshanmeta <540118044@qq.com>
2020-02-05 02:14:57 +0800

    lintcode 1181

---
+++ javascript/1181.diameter-of-binary-tree.js
@@ -0,0 +1,24 @@
+/**
+ * @param root: a root of binary tree
+ * @return: return a integer
+ */
+const diameterOfBinaryTree = function (root) {
+    if(!root){
+        return 0;
+    }
+    return postOrder(root)[0]-1;
+}
+
+function postOrder(root){
+    if(!root){
+        return [0,0];
+    }
+
+    const [rstLeft,maxLeft] = postOrder(root.left);
+    const [rstRight,maxRight] = postOrder(root.right);
+
+    return [
+        Math.max(rstLeft,rstRight,maxLeft+maxRight+1),
+        Math.max(maxLeft,maxRight)+1
+    ]
+}
