39eef574ab5dbdb9b8ddc388b4e22b72b63162b4
jiangshanmeta <540118044@qq.com>
2020-02-05 02:14:49 +0800

    lintcode 1178

---
+++ javascript/1178.student-attendance-record-i.js
@@ -0,0 +1,26 @@
+/**
+ * @param s: a string
+ * @return: whether the student could be rewarded according to his attendance record
+ */
+const checkRecord = function (s) {
+    let ACount = 0;
+    let LCount = 0;
+    for(let i=0;i<s.length;i++){
+        if(s[i] === 'A'){
+            ACount++;
+            LCount = 0;
+            if(ACount>1){
+                return false;
+            }
+        }else if(s[i] === 'L'){
+            LCount++;
+            if(LCount>2){
+                return false;
+            }
+        }else{
+            LCount = 0;
+        }
+    }
+    return true;
+}
+
