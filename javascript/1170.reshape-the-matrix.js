92c8f7d746aff20e5618171da540d6ebc40b19b7
jiangshanmeta <540118044@qq.com>
2020-02-04 15:02:40 +0800

    lintcode 1170

---
+++ javascript/1170.reshape-the-matrix.js
@@ -0,0 +1,26 @@
+/**
+ * @param nums: List[List[int]]
+ * @param r: an integer
+ * @param c: an integer
+ * @return: return List[List[int]]
+ */
+const matrixReshape = function (nums, r, c) {
+    if(nums.length*nums[0].length  !== r*c){
+        return nums;
+    }
+    
+    const C = nums[0].length;
+    const result = [];
+    let index = 0;
+    for(let i=0;i<r;i++){
+        const list = new Array(c);
+        for(let j=0;j<c;j++){
+            const mod = index%C;
+            list[j] = nums[(index-mod)/C][mod];
+            index++;
+        }
+        result.push(list);
+    }
+    return result;
+}
+
