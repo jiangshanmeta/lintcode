0a12065a3c64c738b15367552b3091f481a99a0c
jiangshanmeta <540118044@qq.com>
2020-02-06 14:42:15 +0800

    lintcode 1188

---
+++ javascript/1188.minimum-absolute-difference-in-bst.js
@@ -0,0 +1,27 @@
+/**
+ * @param root: the root
+ * @return: the minimum absolute difference between values of any two nodes
+ */
+const getMinimumDifference = function (root) {
+    const stack = [];
+    let result = Infinity;
+    let last = -Infinity;
+
+    while(root){
+        stack.push(root);
+        root = root.left;
+    }
+
+    while(stack.length){
+        root = stack.pop();
+        result = Math.min(result,root.val-last);
+        last = root.val;
+
+        root = root.right;
+        while(root){
+            stack.push(root);
+            root = root.left;
+        }
+    }
+    return result;
+}
