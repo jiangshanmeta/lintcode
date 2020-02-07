fe65388083d46f1edce0d2cd1c1689ad677b517b
jiangshanmeta <540118044@qq.com>
2020-02-07 16:19:10 +0800

    lintcode 1200

---
+++ javascript/1200.relative-ranks.js
@@ -0,0 +1,20 @@
+/**
+ * @param nums: List[int]
+ * @return: return List[str]
+ */
+const findRelativeRanks = function (nums) {
+    const valueIndexMap = {};
+    for(let i=0;i<nums.length;i++){
+        valueIndexMap[nums[i]] = i;
+    }
+    nums.sort((a,b)=>b-a);
+    const result = new Array(nums.length);
+    result[valueIndexMap[nums[0]]] = "Gold Medal"
+    result[valueIndexMap[nums[1]]] = "Silver Medal"
+    result[valueIndexMap[nums[2]]] = "Bronze Medal"
+    for(let i=3;i<nums.length;i++){
+        result[valueIndexMap[nums[i]]] = ''+(i+1);
+    }
+
+    return result;
+}
