81dfbb8e9c4c7e008a725012a46f2a332b2f44a1
jiangshanmeta <540118044@qq.com>
2020-02-10 13:31:44 +0800

    lintcode 1218

---
+++ javascript/1218.number-complement.js
@@ -0,0 +1,15 @@
+/**
+ * @param num: an integer
+ * @return: the complement number
+ */
+const findComplement = function (num) {
+    let num2 = num;
+    let count = 0;
+    while(num2){
+        count++;
+        num2 >>= 1;
+    }
+
+    return num^( (1<<count) -1)
+}
+
