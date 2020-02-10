a8bf696b4b35c5c7c6af6041c5625a3cba5d754e
jiangshanmeta <540118044@qq.com>
2020-02-10 13:31:21 +0800

    lintcode 1209

---
+++ javascript/1209.construct-the-rectangle.js
@@ -0,0 +1,14 @@
+/**
+ * @param area: web pageâ€™s area
+ * @return: the length L and the width W of the web page you designed in sequence
+ */
+const constructRectangle = function (area) {
+    let w = Math.floor(Math.sqrt(area));
+    while(true){
+        if(area%w === 0){
+            return [area/w,w];
+        }
+        w -= 1;
+    }
+    
+}
