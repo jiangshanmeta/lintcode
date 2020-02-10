4537084b9e53b28b71041cd7607f7e97b71d42f0
jiangshanmeta <540118044@qq.com>
2020-02-10 13:31:28 +0800

    lintcode 1212

---
+++ javascript/1212.max-consecutive-ones.js
@@ -0,0 +1,17 @@
+/**
+ * @param nums: a binary array
+ * @return:  the maximum number of consecutive 1s
+ */
+const findMaxConsecutiveOnes = function (nums) {
+    let max = 0;
+    let count = 0;
+    for(let i=0;i<nums.length;i++){
+        if(nums[i] === 1){
+            count++;
+            max = Math.max(max,count);
+        }else{
+            count = 0;
+        }
+    }
+    return max;
+}
