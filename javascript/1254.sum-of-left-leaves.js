5af644d88fd32714012d587944a7a9c14c7d5a49
jiangshanmeta <540118044@qq.com>
2020-02-13 14:44:05 +0800

    lintcode 1254

---
+++ javascript/1254.sum-of-left-leaves.js
@@ -0,0 +1,18 @@
+/**
+ * @param root: t
+ * @return: the sum of all left leaves
+ */
+const sumOfLeftLeaves = function (root) {
+    let result = 0;
+    function helper(root,isLeft){
+        if(!root.left && !root.right){
+            if(isLeft){
+                result += root.val;
+            }
+        }
+        root.left && helper(root.left,true);
+        root.right && helper(root.right,false);
+    }
+    root && helper(root,false);
+    return result;
+}
