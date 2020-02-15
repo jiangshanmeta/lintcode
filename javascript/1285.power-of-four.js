2cbedc848c45abe05252d992a8dcf9474146540e
jiangshanmeta <540118044@qq.com>
2020-02-15 17:57:30 +0800

    1285

---
+++ javascript/1285.power-of-four.js
@@ -0,0 +1,14 @@
+/**
+ * @param num: an integer
+ * @return: whether the integer is a power of 4
+ */
+const isPowerOfFour = function (num) {
+    if(num<1 || (num&(num-1)) !== 0 ){
+        return false;
+    }
+
+    while(num>1){
+        num >>= 2;
+    }
+    return num === 1;
+}
