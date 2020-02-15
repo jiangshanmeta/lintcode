63c265d896bb3bac5a9a57ebb1e92009561fc700
jiangshanmeta <540118044@qq.com>
2020-02-15 17:57:18 +0800

    1283

---
+++ javascript/1283.reverse-string.js
@@ -0,0 +1,12 @@
+/**
+ * @param s: a string
+ * @return: return a string
+ */
+const reverseString = function (s) {
+    const result = [];
+    let index = s.length-1;
+    while(index>-1){
+        result.push(s[index--]);
+    }
+    return result.join('');
+}
