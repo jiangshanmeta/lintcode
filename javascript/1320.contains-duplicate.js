da9d2f5448a43a2c5f410e6a88e1fd0847d48389
jiangshanmeta <540118044@qq.com>
2020-02-18 19:58:51 +0800

    1320

---
+++ javascript/1320.contains-duplicate.js
@@ -0,0 +1,14 @@
+/**
+ * @param nums: the given array
+ * @return: if any value appears at least twice in the array
+ */
+const containsDuplicate = function (nums) {
+    const map = {};
+    for(let i=0;i<nums.length;i++){
+        if(map[nums[i]]){
+            return true;
+        }
+        map[nums[i]] = true;
+    }
+    return false;
+}
