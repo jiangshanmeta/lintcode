20b871b979f57fc63317d253b2194eae2bed30e4
jiangshanmeta <540118044@qq.com>
2020-02-18 19:58:44 +0800

    1319

---
+++ javascript/1319.contains-duplicate-ii.js
@@ -0,0 +1,16 @@
+/**
+ * @param nums: the given array
+ * @param k: the given number
+ * @return:  whether there are two distinct indices i and j in the array such that nums[i] = nums[j] and the absolute difference between i and j is at most k
+ */
+const containsNearbyDuplicate = function (nums, k) {
+    const lastIndexMap = {};
+    for(let i=0;i<nums.length;i++){
+        if(lastIndexMap[nums[i]] !== undefined && i-lastIndexMap[nums[i]]<=k){
+            return true;
+        }
+        lastIndexMap[nums[i]] = i;
+    }
+    return false;
+}
+
