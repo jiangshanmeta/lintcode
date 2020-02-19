fcb295e9b141301efc415bc1106b255be9c0013e
jiangshanmeta <540118044@qq.com>
2020-02-19 19:47:06 +0800

    1333

---
+++ javascript/1333.reverse-bits.js
@@ -0,0 +1,15 @@
+/**
+ * @param n: an integer
+ * @return: return an integer
+ */
+const reverseBits = function (n) {
+    let result = 0
+    let count = 32
+    while(count){
+        result = (result << 1)|(n&1)
+        n >>=1 
+        count--;
+    }
+    return result >>> 0
+}
+
