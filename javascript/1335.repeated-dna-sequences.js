cb0008fc94a31c8df6e12c2ab82f194c3a051442
jiangshanmeta <540118044@qq.com>
2020-02-19 19:47:18 +0800

    1335

---
+++ javascript/1335.repeated-dna-sequences.js
@@ -0,0 +1,20 @@
+/**
+ * @param s: a string
+ * @return: return List[str]
+ */
+const findRepeatedDnaSequences = function (s) {
+    const map = {};
+    const result = [];
+    for(let i=9;i<s.length;i++){
+        const sub = s.substring(i-9,i+1);
+        if(map[sub] === undefined){
+            map[sub] = 1;
+        }else if(map[sub] === 1){
+            map[sub]++;
+            result.push(sub);
+        }
+    }
+    
+    return result.sort();
+}
+
