de5da9314afb27e249ad062df056b15ebd2dcea0
jiangshanmeta <540118044@qq.com>
2020-02-20 20:20:06 +0800

    1347

---
+++ javascript/1347.factorial-trailing-zeroes.js
@@ -0,0 +1,14 @@
+/**
+ * @param n: a integer
+ * @return: return a integer
+ */
+const trailingZeroes = function (n) {
+    let result = 0;
+    let base = 5;
+    while(base<=n){
+        result += parseInt(n/base);
+        base *= 5;
+    }
+    return result;
+}
+
