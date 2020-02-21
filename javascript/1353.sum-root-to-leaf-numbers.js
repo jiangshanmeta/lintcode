71616f91cfd3d1ca2ce191ad35cd4d16be007a56
jiangshanmeta <540118044@qq.com>
2020-02-21 19:33:48 +0800

    1353

---
+++ javascript/1353.sum-root-to-leaf-numbers.js
@@ -0,0 +1,18 @@
+/**
+ * @param root: the root of the tree
+ * @return: the total sum of all root-to-leaf numbers
+ */
+const sumNumbers = function (root) {
+    let sum = 0;
+    function dfs(node,prefix){
+        prefix = prefix*10+node.val;
+        if(!node.left && !node.right){
+            sum += prefix;
+            return;
+        }
+        node.left && dfs(node.left,prefix);
+        node.right && dfs(node.right,prefix);
+    }
+    root && dfs(root,0);
+    return sum;
+}
