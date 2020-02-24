2ee327492422795f18d57de5cc02629545613e96
jiangshanmeta <540118044@qq.com>
2020-02-24 20:00:41 +0800

    1358

---
+++ javascript/1358.path-sum.js
@@ -0,0 +1,15 @@
+/**
+ * @param root: the tree
+ * @param sum: the sum
+ * @return:  if the tree has a root-to-leaf path 
+ */
+const pathSum = function (root, sum) {
+    if(!root){
+        return false;
+    }
+    if(!root.left && !root.right){
+        return root.val === sum;
+    }
+    return pathSum(root.left,sum-root.val) || pathSum(root.right,sum-root.val);
+}
+
