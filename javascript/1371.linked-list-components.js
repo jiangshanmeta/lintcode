957d77708f30110d8f6afcacfd0b34e3bfeebc2d
jiangshanmeta <540118044@qq.com>
2020-02-25 20:32:14 +0800

    1371

---
+++ javascript/1371.linked-list-components.js
@@ -0,0 +1,27 @@
+/**
+ * @param head: the head
+ * @param G: an array
+ * @return: the number of connected components in G
+ */
+const numComponents = function (head, G) {
+    const map = {};
+    for(let i=0;i<G.length;i++){
+        map[G[i]] = true;
+    }
+    let result = 0;
+    let lastNotInG = true;
+    while(head){
+        if(map[head.val]){
+            if(lastNotInG){
+                result++;
+                lastNotInG = false;
+            }
+        
+        }else{
+            lastNotInG = true;
+        }
+        head = head.next;
+    }
+
+    return result;
+}
