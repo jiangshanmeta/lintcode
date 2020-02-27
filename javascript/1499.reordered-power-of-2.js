42fcfb0b7047f7c082ae12b132cfaeec858db71c
jiangshanmeta <540118044@qq.com>
2020-02-27 21:41:47 +0800

    1499

---
+++ javascript/1499.reordered-power-of-2.js
@@ -0,0 +1,17 @@
+/**
+ * @param N: 
+ * @return: return true or false
+ */
+const reorderedPowerOf2 = function (N) {
+    let power = 1;
+    const candidate = [];
+    const maxPower = Math.pow(10,9);
+    while(power<maxPower){
+        // 2的幂转化为字符串 并案字符排序 便于对比
+        candidate.push(power.toString().split('').sort().join(''));
+        power *= 2;
+    }
+    
+    return candidate.includes(N.toString().split('').sort().join(''));
+}
+
