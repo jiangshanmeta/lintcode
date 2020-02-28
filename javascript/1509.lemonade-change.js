ae9948a923392d2523b9d4d4aa3d44a55ade7130
jiangshanmeta <540118044@qq.com>
2020-02-28 22:15:47 +0800

    1509

---
+++ javascript/1509.lemonade-change.js
@@ -0,0 +1,28 @@
+/**
+ * @param bills: the Bill
+ * @return: Return true if and only if you can provide every customer with correct change.
+ */
+const lemonadeChange = function (bills) {
+    const counts = [0,0,0];
+    for(let i=0;i<bills.length;i++){
+        if(bills[i] === 5){
+            counts[0]++; 
+        }else if(bills[i] === 10){
+            if(counts[0] === 0){
+                return false;
+            }
+            counts[0]--;
+            counts[1]++;
+        }else{
+            if(counts[0]>0 && counts[1]>0){
+                counts[0]--;
+                counts[1]--;
+            }else if(counts[0]>2){
+                counts[0] -= 3;
+            }else{
+                return false;
+            }
+        }
+    }
+    return true;
+}
