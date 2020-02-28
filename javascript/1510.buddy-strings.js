b5de4d725546151dc712f80cc467a4c2586e230e
jiangshanmeta <540118044@qq.com>
2020-02-28 22:15:52 +0800

    1510

---
+++ javascript/1510.buddy-strings.js
@@ -0,0 +1,38 @@
+/**
+ * @param A: string A
+ * @param B: string B
+ * @return: bool
+ */
+const buddyStrings = function (A, B) {
+    if(A.length !== B.length){
+        return false;
+    }
+    
+    let lastIndex = -1;
+    const map = {};
+    let changed = false;
+    let hasDuplicate = false;
+    for(let i=0;i<A.length;i++){
+        if(A[i] !== B[i]){
+            if(changed){
+                return false;
+            }
+            
+            if(lastIndex === -1){
+                lastIndex = i;
+            }else{
+                if(A[i] !== B[lastIndex] || B[i] !== A[lastIndex]){
+                    return false;
+                }
+                changed = true;
+            }
+        }
+        if(!map[A[i]]){
+            map[A[i]] = true;
+        }else{
+            hasDuplicate = true;
+        }
+    }
+    
+    return changed || hasDuplicate;
+}
