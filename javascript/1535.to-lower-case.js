704e95517b159727c0e9055a45e8374925b77f9a
jiangshanmeta <540118044@qq.com>
2020-02-29 12:03:48 +0800

    1535

---
+++ javascript/1535.to-lower-case.js
@@ -0,0 +1,16 @@
+/**
+ * @param str: the input string
+ * @return: The lower case string
+ */
+const toLowerCase = function (str) {
+    const result = [];
+    for(let i=0;i<str.length;i++){
+        const code = str.charCodeAt(i);
+        if(code>64 && code<91){
+            result.push(String.fromCharCode(code+32))
+        }else{
+            result.push(str[i]);
+        }
+    }
+    return result.join('');
+}
