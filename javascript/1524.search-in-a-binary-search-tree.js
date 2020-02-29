c4c1eb78bb4db97e7859def9be8022e51a08e67c
jiangshanmeta <540118044@qq.com>
2020-02-29 12:03:41 +0800

    1524

---
+++ javascript/1524.search-in-a-binary-search-tree.js
@@ -0,0 +1,18 @@
+/**
+ * @param root: the tree
+ * @param val: the val which should be find
+ * @return: the node
+ */
+const searchBST = function (root, val) {
+    while(root){
+        if(root.val>val){
+            root = root.left;
+        }else if(root.val<val){
+            root = root.right;
+        }else{
+            return root;
+        }
+    }
+    return null;
+}
+
