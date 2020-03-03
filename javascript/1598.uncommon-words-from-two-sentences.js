2d11ab1733a0914ebdf99c667ce5bf14ff9e2ae3
jiangshanmeta <540118044@qq.com>
2020-03-03 20:43:58 +0800

    1598

---
+++ javascript/1598.uncommon-words-from-two-sentences.js
@@ -0,0 +1,31 @@
+/**
+ * @param A: Sentence A
+ * @param B: Sentence B
+ * @return: Uncommon Words from Two Sentences
+ */
+const uncommonFromSentences = function (A, B) {
+    const listA = A.split(' ');
+    const listB = B.split(' ');
+    const countByA = countBy(listA);
+    const countByB = countBy(listB);
+    const result = [];
+    for(let i=0;i<listA.length;i++){
+        if(countByA[listA[i]] === 1 && !countByB[listA[i]]){
+            result.push(listA[i]);
+        }
+    }
+    for(let i=0;i<listB.length;i++){
+        if(countByB[listB[i]] === 1 && !countByA[listB[i]]){
+            result.push(listB[i]);
+        }
+    }
+    return result;
+}
+
+function countBy(list){
+    const result = {};
+    for(let i=0;i<list.length;i++){
+        result[list[i]] = (result[list[i]] || 0) + 1;
+    }
+    return result;
+}
