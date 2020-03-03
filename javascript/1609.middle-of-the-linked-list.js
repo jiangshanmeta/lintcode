3e5bc6048e617ea608c6a9873159af544d7bdaf8
jiangshanmeta <540118044@qq.com>
2020-03-03 20:44:10 +0800

    1609

---
+++ javascript/1609.middle-of-the-linked-list.js
@@ -0,0 +1,16 @@
+/**
+ * @param head: the head node
+ * @return: the middle node
+ */
+const middleNode = function (head) {
+    const dummyHead = new ListNode(0);
+    dummyHead.next = head;
+    let fast = dummyHead;
+    let slow = dummyHead;
+    while(fast.next && fast.next.next){
+        fast = fast.next.next;
+        slow = slow.next;
+    }
+    return slow.next;
+}
+
