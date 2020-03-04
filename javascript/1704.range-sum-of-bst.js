9a6c9e3677ed10e25cc5d8bfc18c9ce616b9ec1c
jiangshanmeta <540118044@qq.com>
2020-03-04 20:23:12 +0800

    1704

---
+++ javascript/1704.range-sum-of-bst.js
@@ -0,0 +1,22 @@
+/**
+ * @param root: the root node
+ * @param L: an integer
+ * @param R: an integer
+ * @return: the sum
+ */
+const rangeSumBST = function (root, L, R) {
+    let result = 0;
+    function helper(node){
+        if(node.val>R){
+            node.left && helper(node.left);
+        }else if(node.val<L){
+            node.right && helper(node.right);
+        }else{
+            result += node.val;
+            node.left && helper(node.left);
+            node.right && helper(node.right);
+        }
+    }
+    root && helper(root);
+    return result;
+}
