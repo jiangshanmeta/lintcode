86d3bbab2c0308173d4eaac7755a9556fa7ac7aa
jiangshanmeta <540118044@qq.com>
2020-03-04 20:23:01 +0800

    1665

---
+++ javascript/1665.calculate-number.js
@@ -0,0 +1,23 @@
+/**
+ * @param num: the num
+ * @return: the array subject to the description
+ */
+const calculateNumber = function (num) {
+    const stack = [];
+    let count = 0;
+    let index = 1;
+    while(num){
+        if(num&1){
+            stack.push(index);
+            count++;
+        }
+        index++;
+        num >>= 1;
+    }
+    const result = [count];
+    for(let i=stack.length-1;i>-1;i--){
+        result.push(index-stack[i]);
+    }
+    return result;
+}
+
