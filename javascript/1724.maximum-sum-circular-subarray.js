24294fe6f8d8dd079f8b2a5abac30c994135f0c3
jiangshanmeta <540118044@qq.com>
2020-03-05 20:19:39 +0800

    1724

---
+++ javascript/1724.maximum-sum-circular-subarray.js
@@ -0,0 +1,32 @@
+/**
+ * @param A: the array
+ * @return: Maximum Sum Circular Subarray
+ */
+const maxSubarraySumCircular = function (A) {
+    const rightMax = new Array(A.length);
+    rightMax[rightMax.length-1] = A[A.length-1];
+    let suffix = A[A.length-1];
+    for(let i=A.length-2;i>0;i--){
+        suffix += A[i];
+        rightMax[i] = Math.max(rightMax[i+1],suffix);
+    }
+    rightMax.push(0);
+    let result = -Infinity;
+    let sum = 0;
+    let prefix = 0;
+    for(let i=0;i<A.length;i++){
+        sum += A[i];
+        if(prefix<0){
+            prefix = A[i];
+        }else{
+            prefix += A[i];
+        }
+        result = Math.max(
+            result,
+            prefix,
+            sum+rightMax[i+1]
+        );
+        
+    }
+    return result;
+}
