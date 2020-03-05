58f5944d1662e2adf6a5f635de7ba76261691b68
jiangshanmeta <540118044@qq.com>
2020-03-05 20:19:34 +0800

    1721

---
+++ javascript/1721.minimum-add-to-make-parentheses-valid.js
@@ -0,0 +1,16 @@
+/**
+ * @param S: the given string
+ * @return: the minimum number of parentheses we must add
+ */
+const minAddToMakeValid = function (S) {
+    const stack = [];
+    for(let i=0;i<S.length;i++){
+        if(S[i] === ')' && stack.length && stack[stack.length-1] === '('){
+            stack.pop();
+        }else{
+            stack.push(S[i]);
+        }
+    }
+    return stack.length;
+}
+
