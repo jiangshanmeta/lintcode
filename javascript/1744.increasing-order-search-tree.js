3a37a7c1211a96216c13f8809de3cdc78f171222
jiangshanmeta <540118044@qq.com>
2020-03-06 19:33:21 +0800

    1744

---
+++ javascript/1744.increasing-order-search-tree.js
@@ -0,0 +1,22 @@
+/**
+ * @param root: a binary search tree
+ * @return: Root of a tree
+ */
+const increasingBST = function (root) {
+    if(!root){
+        return root;
+    }
+    root.right = increasingBST(root.right);
+
+    if(root.left){
+        const leftRoot = increasingBST(root.left);
+        let last = leftRoot;
+        while(last.right){
+            last = last.right;
+        }
+        last.right = root;
+        root.left = null;
+        return leftRoot;
+    }
+    return root;
+}
