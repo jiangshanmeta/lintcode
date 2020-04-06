170ff56834f2dffad2cfe7ca89d0730aaca52ec6
jiangshanmeta <540118044@qq.com>
2020-04-06 14:25:23 +0800

    764

---
+++ python/0764.calculate-circumference-and-area.py
@@ -0,0 +1,8 @@
+class Solution:
+    """
+    @param r: a Integer represent radius
+    @return: the circle's circumference nums[0] and area nums[1]
+    """
+    def calculate(self, r):
+        # write your code here
+        return [2*3.14*r,3.14*(r**2)]
