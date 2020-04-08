940e7b4fe18242bf1c3a27de06583018be673a73
jiangshanmeta <540118044@qq.com>
2020-04-08 21:35:52 +0800

    691

---
+++ javascript/0691.recover-binary-search-tree.js
@@ -0,0 +1,31 @@
+/**
+ * @param root: the given tree
+ * @return: the tree after swapping
+ */
+const bstSwappedNode = function (root) {
+    const sequence = [];
+    inOrder(root,sequence);
+
+    for(let i=0;i<sequence.length-1;i++){
+        // 找到那个大的
+        if(sequence[i].val>sequence[i+1].val){
+            // 找到那个小的
+            let j = i+1;
+            for(let k=j;k<sequence.length;k++){
+                if(sequence[k].val<sequence[k-1].val){
+                    j = k;
+                }
+            }
+            const tmp = sequence[i].val;
+            sequence[i].val = sequence[j].val;
+            sequence[j].val = tmp;
+        }
+    }
+    return root;
+}
+
+function inOrder(root,sequence){
+    root.left && inOrder(root.left,sequence);
+    sequence.push(root);
+    root.right && inOrder(root.right,sequence);
+}
