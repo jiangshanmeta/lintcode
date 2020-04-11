f1fc9891933e964ec6e78d8aca818796d078044c
jiangshanmeta <540118044@qq.com>
2020-04-11 12:35:34 +0800

    1186

---
+++ python/1186.encode-and-decode-tinyurl.py
@@ -0,0 +1,8 @@
+class Solution:
+    def encode(self, longUrl):
+        # Encodes a URL to a shortened URL.
+        return longUrl
+
+    def decode(self, shortUrl):
+        # Decodes a shortened URL to its original URL.
+        return shortUrl
