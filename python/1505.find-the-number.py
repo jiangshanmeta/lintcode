797ce3f785778822c71059e97f7b2dbb85808aca
jiangshanmeta <540118044@qq.com>
2020-04-14 20:43:32 +0800

    1505

---
+++ python/1505.find-the-number.py
@@ -0,0 +1,8 @@
+class Solution:
+    """
+    @param nums: a integer array
+    @param k: a integer
+    @return: return true or false denoting if the element is present in the array or not
+    """
+    def findnNumber(self, nums, k):
+        return k in nums
