c5fd97dc924e1fe84057dc71f8643f7e6e7cda19
jiangshanmeta <540118044@qq.com>
2022-10-19 09:32:10 +0800

    13

---
+++ src/0013.implement-strstr/solution.ts
@@ -0,0 +1,10 @@
+export class Solution {
+    /**
+     * @param source:
+     * @param target:
+     * @return: return the index
+     */
+    strStr (source: string, target: string): number {
+        return source.indexOf(target);
+    }
+}
