71d6414353a56a433a7e10e8ce8e5d5fd5746429
jiangshanmeta <540118044@qq.com>
2022-10-20 14:13:56 +0800

    297

---
+++ src/0297.find-the-maximum/solution.ts
@@ -0,0 +1,9 @@
+export class Solution {
+    /**
+     * @param nums: the list of numbers
+     * @return: return the maximum number.
+     */
+    maxNum (nums: number[]): number {
+        return Math.max(...nums);
+    }
+}
