47d1fccd7499682bc4d5ef5629626bd12d3be584
jiangshanmeta <540118044@qq.com>
2022-11-22 20:06:00 +0800

    68

---
+++ src/0068.binary-tree-postorder-traversal/solution.ts
@@ -0,0 +1,33 @@
+class TreeNode {
+    val :number;
+    left :TreeNode;
+    right :TreeNode;
+    constructor(val :number) {
+        this.val = val;
+        this.left = this.right = null;
+    }
+}
+
+export class Solution {
+  /**
+   * @param root: A Tree
+   * @return: Postorder in ArrayList which contains node values.
+   */
+  postorderTraversal(root: TreeNode | null): number[] {
+    const result:number[] = [];
+    if(root){
+      this.recursivePost(root,result)
+    }
+    return result
+  }
+
+  recursivePost(root:TreeNode,result:number[]){
+    if(root.left){
+      this.recursivePost(root.left,result)
+    }
+    if(root.right){
+      this.recursivePost(root.right,result)
+    }
+    result.push(root.val)
+  }
+}
