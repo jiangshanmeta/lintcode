1b223d6e9772d5b7af7b8f0378ad0237f482bb36
jiangshanmeta <540118044@qq.com>
2022-11-22 20:05:48 +0800

    55

---
+++ src/0055.compare-strings/solution.ts
@@ -0,0 +1,18 @@
+export class Solution {
+    /**
+     * @param a: A string
+     * @param b: A string
+     * @return: if string A contains all of the characters in B return true else return false
+     */
+    compareStrings(a: string, b: string): boolean {
+      const counts = new Array<number>(26).fill(0);
+      for(let i=0;i<a.length;i++){
+        counts[a.charCodeAt(i)-65]++;
+      }
+      for(let i=0;i<b.length;i++){
+        counts[b.charCodeAt(i)-65]--;
+      }
+      return counts.every(item=>item>=0)
+    }
+  }
+  
