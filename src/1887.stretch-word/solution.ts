839521c55b0084b1fbe23f28e134513f697a3740
jiangshanmeta <540118044@qq.com>
2022-11-29 11:31:07 +0800

    1887

---
+++ src/1887.stretch-word/solution.ts
@@ -0,0 +1,22 @@
+export class Solution {
+    /**
+     * @param s: the string
+     * @return: The numbers of strings
+     */
+    stretchWord(s: string): number {
+      let result = 1;
+      let index = 0;
+      while(index<s.length){
+        const c = s[index++];
+        let count = 1;
+        while(index<s.length && s[index] === c){
+          count++;
+          index++;
+        }
+        if(count>=2){
+          result *= 2;
+        }
+      }
+      return result;
+    }
+  }
