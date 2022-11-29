3e816beae046ba9db813b0aca6068c4f2e8558fc
jiangshanmeta <540118044@qq.com>
2022-11-29 11:30:37 +0800

    97

---
+++ src/0097.maximum-depth-of-binary-tree/solution.ts
@@ -0,0 +1,22 @@
+class TreeNode {
+    val :number;
+    left :TreeNode;
+    right :TreeNode;
+    constructor(val :number) {
+        this.val = val;
+        this.left = this.right = null;
+    }
+}
+
+export class Solution {
+  /**
+   * @param root: The root of binary tree.
+   * @return: An integer
+   */
+  maxDepth(root: TreeNode | null): number {
+    if(!root){
+      return 0;
+    }
+    return Math.max(this.maxDepth(root.left),this.maxDepth(root.right))+1;
+  }
+}
