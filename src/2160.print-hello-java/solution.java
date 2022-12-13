52a3e9ac051ff93e9eaa358a106e1e972ca86bb0
jiangshanmeta <540118044@qq.com>
2022-12-13 20:34:09 +0800

    2160

---
+++ src/2160.print-hello-java/solution.java
@@ -0,0 +1,9 @@
+public class Solution {
+    /* Your first java code
+     * print Hello Java to console
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello Java");
+		
+    }
+}
