0bf495ba8cb4d7668840ad3c1c5bbd187e9346a6
jiangshanmeta <540118044@qq.com>
2022-12-13 20:34:23 +0800

    2165

---
+++ src/2165.calculate-the-values-of-123-n-values/solution.java
@@ -0,0 +1,10 @@
+public class Solution {
+    public int add(int number) {
+        int sum = 0;
+        for(int i=1;i<=number;i++){
+            sum += i;
+        }
+
+        return sum;
+    }
+}
