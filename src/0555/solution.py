45521ffced78a183304765852484b7940ca1218a
jiangshanmeta <540118044@qq.com>
2023-07-15 13:14:44 +0800

    555

---
+++ src/0555/solution.py
@@ -0,0 +1,43 @@
+import uuid
+
+
+class CountingBloomFilter:
+    """
+    @param: k: An integer
+    """
+    def __init__(self, k):
+        self.size = 5000000
+        self.bit_array = [0]*self.size
+
+        self.hash_funcs = []
+        for _ in range(k):
+            salt = str(uuid.uuid4())
+            self.hash_funcs.append(lambda x: hash(salt+x))
+    """
+    @param: word: A string
+    @return: nothing
+    """
+    def add(self, word):
+        for hash_func in self.hash_funcs:
+            index = hash_func(word)%self.size
+            self.bit_array[index] += 1
+
+    """
+    @param: word: A string
+    @return: nothing
+    """
+    def remove(self, word):
+        for hash_func in self.hash_funcs:
+            index = hash_func(word)%self.size
+            self.bit_array[index] -= 1
+
+    """
+    @param: word: A string
+    @return: True if contains word
+    """
+    def contains(self, word):
+        for hash_func in self.hash_funcs:
+            index = hash_func(word)%self.size
+            if self.bit_array[index] == 0:
+                return False
+        return True
