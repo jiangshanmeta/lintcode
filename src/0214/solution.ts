4d85d23be35fd1bc966b2b91072deff339a66dd7
jiangshanmeta <540118044@qq.com>
2023-09-03 18:10:41 +0800

    214

---
+++ src/0214/solution.ts
@@ -0,0 +1,9 @@
+export class Solution {
+    /**
+     * @param a: an array of float numbers
+     * @return: a float number
+     */
+    maxOfArray (a: number[]): number {
+        return Math.max(...a);
+    }
+}
