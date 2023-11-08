46e6f923275cb817fdd509b881f1761680a8990a
jiangshanmeta <540118044@qq.com>
2023-11-08 21:01:40 +0800

    3064

---
+++ src/3064/solution.php
@@ -0,0 +1,14 @@
+<?php
+class Solution{
+	function data_portfolio(){
+        $arr = array(
+			'int' => 1,
+			'float' => 1.0,
+			'str' => 'a',
+			'bool' => True,
+			'null' => NULL
+		);
+		return $arr;
+	}
+}
+?>
