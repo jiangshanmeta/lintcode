17406473e151215a9d1cf3ce88c917f492b64337
jiangshanmeta <540118044@qq.com>
2023-11-08 21:01:46 +0800

    3067

---
+++ src/3067/solution.php
@@ -0,0 +1,19 @@
+<?php
+class Solution{
+	function addition($a, $b){
+		return $a+$b;
+	}
+	
+	function subtraction($a, $b){
+		return $a-$b;
+	}
+	
+	function multiplication($a, $b){
+		return $a*$b;
+	}
+	
+	function division($a, $b){
+		return $a/$b;
+	}
+}
+?>
