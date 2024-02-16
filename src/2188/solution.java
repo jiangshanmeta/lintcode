27473b814d2b2a4083f2a07006aa08546a166d57
jiangshanmeta <540118044@qq.com>
2024-02-16 12:02:00 +0800

    2188

---
+++ src/2188/solution.java
@@ -0,0 +1,10 @@
+public class Solution {
+    /**
+     * @param str: represents the string passed in
+     * @return: represents the return of a character with index position 1
+     */
+    public char getIndexOne(String str) {
+        return str.charAt(1);
+        
+    }
+}
