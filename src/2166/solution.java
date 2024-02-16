1341760d81fd8634463bebb02dda258afc748b9e
jiangshanmeta <540118044@qq.com>
2024-02-16 12:01:46 +0800

    2166

---
+++ src/2166/solution.java
@@ -0,0 +1,17 @@
+public class Solution {
+    public int addition(int a, int b) {
+        return a+b;
+    }
+
+    public int subtraction(int a, int b) {
+        return a-b;
+    }
+
+    public int multiplication(int a, int b) {
+        return a*b;
+    }
+
+    public float division(int a, int b) {
+        return (float)a/(float)b;
+    }
+}
