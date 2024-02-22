5f397501e4cf3b9c0a2f71b3fb0b8ce2254125c6
jiangshanmeta <540118044@qq.com>
2024-02-22 18:48:47 +0800

    2201

---
+++ src/2201/solution.java
@@ -0,0 +1,11 @@
+public class Solution {
+    /**
+     * @param str: represents the string passed in
+     * @return: represents the element whose index
+     *          is the last digit
+     */
+    public char getLastIndex(String str) {
+        return str.charAt(str.length()-1);
+        
+    }
+}
