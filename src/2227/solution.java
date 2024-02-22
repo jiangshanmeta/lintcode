6e4d6e91c8bd0dbea326cc8776cc4f1360e58828
jiangshanmeta <540118044@qq.com>
2024-02-22 18:49:01 +0800

    2227

---
+++ src/2227/solution.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // write your code here
+        // read data from console
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        // output the answer to the console according to the
+        // requirements of the question
+        System.out.println(n<<3);
+    }
+}
