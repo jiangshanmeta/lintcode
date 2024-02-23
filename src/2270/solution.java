280392ee446b21a8b47a0953f4108656475eb2f4
jiangshanmeta <540118044@qq.com>
2024-02-23 19:33:58 +0800

    2270

---
+++ src/2270/solution.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // write your code here
+        // read data from console
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        // output the answer to the console according to the
+        // requirements of the question
+        System.out.println(a+b);
+        System.out.println(a-b);
+        System.out.println(a*b);
+        System.out.println(a/b);
+
+    }
+}
