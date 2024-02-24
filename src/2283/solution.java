0deb60fde084186792f5c6fdea19c67422bb9427
jiangshanmeta <540118044@qq.com>
2024-02-24 16:30:21 +0800

    2283

---
+++ src/2283/solution.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int r = Integer.parseInt(args[0]);
+        // write your code here
+        // please print the perimeter of the circle
+
+        System.out.printf( "%.2f" ,Math.PI*(double)r*2.0 );
+    }
+}
