85885bdfd8ade3233140821a0e9e88ba62fe514c
jiangshanmeta <540118044@qq.com>
2024-02-27 23:23:00 +0800

    2373

---
+++ src/2373/solution.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+
+        for (int i = 1; i <= n; i++) {
+            System.out.println(i);
+            if(i == m){
+                break;
+            }
+
+        }
+    }
+}
