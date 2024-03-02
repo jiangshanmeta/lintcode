960da05798b61476143fc08a0494b8fb52aa90a9
jiangshanmeta <540118044@qq.com>
2024-03-02 12:31:55 +0800

    2369

---
+++ src/2369/solution.java
@@ -0,0 +1,6 @@
+final public class Solution {
+    final private String SENTENCES = "TO BE OR NOT TO BE";
+    final public void test(String name){
+        System.out.println(name + " said: " + SENTENCES);
+    }
+}
