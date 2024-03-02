1c69c672e8c02643cb9bc5faf36e5a1d9a9b4b05
jiangshanmeta <540118044@qq.com>
2024-03-02 12:32:00 +0800

    2374

---
+++ src/2374/solution.java
@@ -0,0 +1,8 @@
+public class Solution {
+    String a = "hello2";
+    final String b = "hello";
+    String d = "hello";
+    // Do not modify the code below
+    String c = b + "2";
+    String e = d + "2";
+}
