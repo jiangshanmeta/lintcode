445aaf29daefa5617d87eec2b4219abc18caec12
jiangshanmeta <540118044@qq.com>
2024-03-05 23:32:55 +0800

    2393

---
+++ src/2393/solution.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+import java.util.Iterator;
+
+
+public class Solution {
+    /**
+     * @param list: Represents the incoming set
+     */
+    public void traverseList(ArrayList<String> list) {
+        for(String str:list){
+            System.out.println(str);
+        }
+        
+    }
+}
