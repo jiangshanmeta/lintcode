cf1b1604f8002ac7b0e7a64d7359fc25808a79db
jiangshanmeta <540118044@qq.com>
2024-03-14 20:41:15 +0800

    2663

---
+++ src/2663/solution.java
@@ -0,0 +1,17 @@
+class Animal {
+    int id;
+    String species;
+    String color;
+
+    public Animal(int id, String species, String color) {
+        this.id = id;
+        this.species = species;
+        this.color = color;
+    }
+
+    public Animal(String species, String color) {
+        this.species = species;
+        this.color = color;
+    }
+
+}
