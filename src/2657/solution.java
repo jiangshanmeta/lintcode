7e6c4a1dfa11e830c817b46a6c837ad464d8920a
jiangshanmeta <540118044@qq.com>
2024-03-14 20:41:10 +0800

    2657

---
+++ src/2657/solution.java
@@ -0,0 +1,10 @@
+public class Solution {
+    public void Add(int age,int height){
+        System.out.println("I am " + age + " years old,I am " + height +  " cm tall");
+    } 
+
+    public void Add(String fn, String ln){
+        System.out.println("My name is " +fn + ln);
+    }
+	
+}
