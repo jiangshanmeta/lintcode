c7485ced2c912ce18ab9fd0b78981f2b3ccca144
jiangshanmeta <540118044@qq.com>
2024-03-17 12:50:58 +0800

    2821

---
+++ src/2821/solution.java
@@ -0,0 +1,12 @@
+public class Main {
+	public static void main(String[] args) {
+		for(int i=0;i<10;i++){
+			if(i ==6){
+				continue;
+			}
+			System.out.print(i);
+			System.out.print(" ");
+		}
+		
+	}
+}
