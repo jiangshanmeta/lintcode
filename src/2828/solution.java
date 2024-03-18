4769bb7756214e81c01e82e469c8560fc22af9e1
jiangshanmeta <540118044@qq.com>
2024-03-18 12:46:30 +0800

    2828

---
+++ src/2828/solution.java
@@ -0,0 +1,8 @@
+public class Main {
+	public static void main(String[] args) {
+        String str1 = "I love ";
+		String str2 = "China";
+		// write your code here
+		System.out.println(str1.toUpperCase()+str2.toUpperCase());
+	}
+}
