26628d843412ee7207cf8a3267e8830ab9252d1a
jiangshanmeta <540118044@qq.com>
2024-03-19 21:15:50 +0800

    2831

---
+++ src/2831/solution.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        String str = "anihC";
+        StringBuilder sBuilder = new StringBuilder(str);
+        sBuilder.reverse();
+        System.out.println(sBuilder.toString());
+
+    }
+}
