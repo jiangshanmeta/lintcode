2fbc460872f95983e9cbd66abdf7877d87a54458
jiangshanmeta <540118044@qq.com>
2024-03-21 19:32:41 +0800

    2842

---
+++ src/2842/solution.java
@@ -0,0 +1,13 @@
+public class Student {
+    public String name;
+    public int age;
+    public double height;
+    
+    // write your code here
+    Student(){
+        name = "Tom";
+        age = 20;
+        height = 180.0;
+    }
+    
+}
