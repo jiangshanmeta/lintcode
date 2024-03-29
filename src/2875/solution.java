252c931b60a0d349419e6a3c8c4496d3a5f110d9
jiangshanmeta <540118044@qq.com>
2024-03-29 20:01:38 +0800

    2875

---
+++ src/2875/solution.java
@@ -0,0 +1,9 @@
+public class Solution {
+    public void printNum(int num) {
+        for (int i = 0; i < num; i++) {
+            TestB test = new TestB();
+        }
+        System.out.println(TestB.num);
+
+    }
+}
