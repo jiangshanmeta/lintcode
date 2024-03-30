0f0377e777c6979ca1b2594f08e01211f1a2965b
jiangshanmeta <540118044@qq.com>
2024-03-30 19:53:05 +0800

    2880

---
+++ src/2880/solution.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+public class Solution {
+    public static void putMap(Map<String, Integer> map) {
+        map.put("January", 31);
+        map.put("February", 28);
+        map.put("March", 31);
+        map.put("April", 30);
+        map.put("May", 31);
+        map.put("June", 30);
+        map.put("July", 31);
+        map.put("August", 31);
+        map.put("September", 30);
+        map.put("October", 31);
+        map.put("November", 30);
+        map.put("December", 31);
+
+    }
+
+}
