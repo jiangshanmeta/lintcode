9c15d1f2ed3ab671de2a82ad972a65962da45a82
jiangshanmeta <540118044@qq.com>
2024-05-15 23:20:05 +0800

    3192

---
+++ src/3192/solution.php
@@ -0,0 +1,12 @@
+<?php
+require "Interface.php";
+// write your code here  
+class Solution {
+	public function print1(){
+		return "Solution implements Interface1";
+	}
+	public function print2(){
+		return "Solution implements Interface2";
+	}
+}
+?>
